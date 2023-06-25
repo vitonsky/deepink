54636c5c7eac42d2b3a6455bb3b42a5b8fc05421
Robert Vitonsky <rob@vitonsky.net>
2023-06-26 01:00:34 +0300

    chore: implement notes navigation

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -1,21 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { cn } from '@bem-react/classname';
 
-import { notes } from '../../core/Note';
+import { INote, notes } from '../../core/Note';
 
 import './App.css';
 
 const cnApp = cn('App');
 
+const getNoteTitle = (note: INote) => note.title ?? note.text.slice(0, 35);
+
 export const App: FC = () => {
+	const [tabs, setTabs] = useState<number[]>([]);
+	const [tab, setTab] = useState<number | null>(null);
+
+	const onNoteClick = useCallback((id: number) => {
+		setTabs((state) => state.includes(id) ? state : [...state, id]);
+		setTab(id);
+	}, []);
+
+	const activeNote = tab !== null ? notes[tab] : null;
+
 	return <div className={cnApp()}>
 		<div className={cnApp('SideBar')}>
 			{notes.map((note, id) => {
-				return <li key={id}>{note.title ?? note.text.slice(0, 35)}</li>;
+				return <li key={id} onClick={() => onNoteClick(id)}>{getNoteTitle(note)}</li>;
 			})}
 		</div>
 		<div className={cnApp('ContentBlock')}>
-			Content
+			<div>
+				{tabs.map((noteId) => {
+					const note = notes[noteId];
+					if (!note) return undefined;
+
+					return <li key={noteId}>{getNoteTitle(note)} <span>[x]</span></li>;
+				})}
+			</div>
+			<div>
+				{activeNote && <div>
+					<h2>{getNoteTitle(activeNote)}</h2>
+					<div>{activeNote.text}</div>
+				</div>}
+			</div>
 		</div>
 	</div>;
 };
