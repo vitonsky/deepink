af3c4a7c39643a0bec97469e37bf014b0b31e75c
Robert Vitonsky <rob@vitonsky.net>
2023-06-26 14:05:37 +0300

    refactor: introduce notes registry

--- src/app/App/index.tsx
+++ src/app/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import { cn } from '@bem-react/classname';
 import { cnTheme } from 'react-elegant-ui/esm/theme';
 import { theme } from 'react-elegant-ui/esm/theme/presets/default';
@@ -7,7 +7,7 @@ import { Menu } from 'react-elegant-ui/esm/components/Menu/Menu.bundle/desktop';
 import { TabsMenu } from 'react-elegant-ui/esm/components/TabsMenu/TabsMenu.bundle/desktop';
 import { Icon } from 'react-elegant-ui/esm/components/Icon/Icon.bundle/desktop';
 
-import { IEditableNote, INote, notes } from '../../core/Note';
+import { IEditableNote, INote, notesRegistry } from '../../core/Note';
 
 import './App.css';
 import { NoteEditor } from './NoteEditor';
@@ -17,19 +17,27 @@ export const cnApp = cn('App');
 export const getNoteTitle = (note: INote) => note.title ?? note.text.slice(0, 35);
 
 export const App: FC = () => {
-	const [tabs, setTabs] = useState<number[]>([]);
-	const [tab, setTab] = useState<number | null>(null);
+	const [tabs, setTabs] = useState<string[]>([]);
+	const [tab, setTab] = useState<string | null>(null);
+	const [notes, setNotes] = useState<IEditableNote[]>([]);
 
-	// const [notesSlice, setNotesSlice] = useState(notes);
+	const updateNotes = useCallback(async () => {
+		const notes = await notesRegistry.getNotes();
+		setNotes(notes);
+	}, []);
 
-	const onNoteClick = useCallback((itemId: string) => {
-		const id = Number(itemId);
+	// Init
+	useEffect(() => {
+		updateNotes();
+	}, []);
+
+	const onNoteClick = useCallback((id: string) => {
 		setTabs((state) => (state.includes(id) ? state : [...state, id]));
 		setTab(id);
 	}, []);
 
 	const closeNote = useCallback(
-		(id: number) => {
+		(id: string) => {
 			const tabIndex = tabs.indexOf(id);
 
 			// Change tab if it is current tab
@@ -48,19 +56,10 @@ export const App: FC = () => {
 		[tabs],
 	);
 
-	// TODO: implement real logic
 	// Simulate note update
 	const updateNote = useCallback(async (note: IEditableNote) => {
-		await new Promise((res) => setTimeout(res, 1000));
-
-		const noteInDB = notes[note.id];
-		if (!noteInDB) {
-			console.warn('Note not found', note.id);
-			return;
-		}
-
-		noteInDB.title = note.title;
-		noteInDB.text = note.text;
+		await notesRegistry.updateNote(note);
+		updateNotes();
 	}, []);
 
 	return (
@@ -68,7 +67,8 @@ export const App: FC = () => {
 			<div className={cnApp('SideBar')}>
 				<div className={cnApp('SideBarControls')}>
 					<Button view='action' onPress={() => {
-						notes.unshift({ text: '123', title: '' });
+						notesRegistry.addNote({ text: '' });
+						updateNotes();
 					}}>New note</Button>
 				</div>
 
@@ -76,15 +76,14 @@ export const App: FC = () => {
 					{/* TODO: implement dragging and moving items */}
 					<Menu
 						onPick={onNoteClick}
-						cursorIndex={tab ?? undefined}
-						items={notes.map((note, id) => {
+						items={notes.map((note) => {
 							return {
-								id: String(id),
-								content: getNoteTitle(note),
+								id: note.id,
+								content: note.title || 'Unnamed note',
 								addonProps: {
 									className: cnApp('NoteItem', {
-										active: id === tab,
-										opened: tabs.indexOf(id) !== -1,
+										active: note.id === tab,
+										opened: tabs.indexOf(note.id) !== -1,
 									}),
 								},
 							};
@@ -99,18 +98,19 @@ export const App: FC = () => {
 					layout="horizontal"
 					dir="horizontal"
 					activeTab={String(tab)}
-					setActiveTab={(id) => {
-						setTab(Number(id));
-					}}
+					setActiveTab={setTab}
 					tabs={tabs.map((noteId) => {
 						// TODO: handle case when object not found
-						const note = notes[noteId];
+						const note = notes.find((note) => note.id === noteId);
+						if (!note) {
+							throw new Error('Note not found');
+						}
 
 						return {
 							id: String(noteId),
 							content: (
 								<span>
-									{getNoteTitle(note)}{' '}
+									{note.title}{' '}
 									<span
 										onClick={(evt) => {
 											evt.stopPropagation();
@@ -127,7 +127,7 @@ export const App: FC = () => {
 				/>
 				<div className={cnApp('NoteEditors')}>
 					{tabs.map((id) => {
-						const noteObject = notes[id];
+						const noteObject = notes.find((note) => note.id === id);
 						if (!noteObject) return null;
 
 						const note = {
--- src/core/Note.ts
+++ src/core/Note.ts
@@ -1,29 +1,49 @@
+import { getNoteTitle } from '../app/App';
+
 export type INote = {
 	title?: string;
 	text: string;
 	date?: number;
-}
+};
 
 export type IEditableNote = {
-	id: number;
+	id: string;
 	title: string;
 	text: string;
+};
+
+const delay = (time: number) => new Promise((res) => setTimeout(res, time));
+export class NotesRegistry {
+	private notes: { id: string; note: INote }[] = [];
+
+	public async getNotes(): Promise<IEditableNote[]> {
+		await delay(500);
+		return this.notes.map((note) => ({
+			id: note.id,
+			title: getNoteTitle(note.note),
+			...note.note,
+		}));
+	}
+
+	private uidCounter = 0;
+	public async addNote(note: INote) {
+		await delay(30);
+		const id = ++this.uidCounter + '_note';
+
+		this.notes.push({ id, note });
+
+		return id;
+	}
+
+	public async updateNote(updatedNote: IEditableNote) {
+		await delay(100);
+
+		const note = this.notes.find((note) => note.id === updatedNote.id);
+		if (!note) return;
+
+		const { id, ...updatedData } = updatedNote;
+		Object.assign(note.note, updatedData);
+	}
 }
 
-export const notes: INote[] = [
-	{
-		text: `Nulla quia sunt vel ea est ea. Qui reprehenderit quibusdam et quia. Natus minima culpa dolor. Velit aliquid cum aut enim molestias voluptatem consequatur culpa. Voluptatem impedit tenetur quisquam et hic.
- 
-	Aliquam corporis laborum molestiae. Autem necessitatibus corporis ex sunt eveniet quo et. Id provident animi. Sit modi saepe error doloribus est voluptas rerum nemo.
-	 
-	Nihil eum ducimus consequatur reiciendis rem accusamus aut. At nobis adipisci qui quidem voluptatem. Ut velit consequuntur iusto nihil sint nisi saepe cumque sint. Voluptatem ex dicta sapiente eum sit.`
-	},
-	{
-		title: 'Sint voluptas sunt',
-		text: `Sint voluptas sunt et soluta perferendis qui animi. Et nam saepe incidunt dignissimos reprehenderit. In corrupti dolorum excepturi alias rerum est qui est sunt. Atque ipsa aperiam consequatur odit dolorum sit.
- 
-Quos aut consequuntur quae rem accusantium ut cum ipsam aperiam. Rerum totam hic corrupti eaque. Quaerat iusto aliquam rerum modi maiores aut suscipit ratione eveniet. Dolorum est quos voluptatem. Ex minus dolorem omnis optio aperiam porro est. Vel voluptate dolorum quibusdam excepturi quae aut illum.
- 
-Fugiat quia hic tempora fugit illum repellat sint. Culpa et rem quos quisquam quaerat ipsum ea. Vitae delectus assumenda iusto aut qui omnis. Id dolorum placeat modi. Exercitationem omnis odit quia.`
-	}
-].concat(Array(100).fill({ text: 'Test markdown content' }));
+export const notesRegistry = new NotesRegistry();
