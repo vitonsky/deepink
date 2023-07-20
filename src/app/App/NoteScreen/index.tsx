62f0922eddd5e4bf8ce4b186b35c878045abc4d1
Robert Vitonsky <rob@vitonsky.net>
2023-07-21 02:08:25 +0300

    feat(#9): supports checkbox with number lists

--- src/app/App/NoteScreen/index.tsx
+++ src/app/App/NoteScreen/index.tsx
@@ -131,7 +131,7 @@ export const NoteScreen: FC<NoteScreenProps> = ({ note, update }) => {
 			if (start === undefined || end === undefined) return;
 
 			const nodeValue = text.slice(start, end);
-			const updatedValue = nodeValue.replace(/(\s*- \[)(x|\s)(\])/, `$1${isChecked ? 'x' : ' '}$3`);
+			const updatedValue = nodeValue.replace(/(?<start>\s*(-|\d+\.) \[)(x|\s)(?<end>\])/, `$<start>${isChecked ? 'x' : ' '}$<end>`);
 			if (updatedValue === nodeValue) return;
 
 			const updatedText = text.slice(0, start) + updatedValue + text.slice(end);
