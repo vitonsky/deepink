147b0466b5a0e17b6789f593a95a9f2bf11b6212
Robert Vitonsky <rob@vitonsky.net>
2023-07-23 00:39:29 +0300

    chore(#9): add issue to TODO comments

--- src/app/App/MonakoEditor/extensions/MarkdownAddons.ts
+++ src/app/App/MonakoEditor/extensions/MarkdownAddons.ts
@@ -3,8 +3,7 @@ import { MonacoMarkdownExtension } from "monaco-markdown";
 import { EditorExtension } from ".";
 
 export const MarkdownAddons: EditorExtension = (editorInstance) => {
-	// TODO: adopt and inline code from https://github.com/yzhang-gh/vscode-markdown/blob/83f602638bf96f5249e3b9856baf9f31b933fad1/src/listEditing.ts
-
+	// TODO: #13 adopt and inline code from https://github.com/yzhang-gh/vscode-markdown/blob/83f602638bf96f5249e3b9856baf9f31b933fad1/src/listEditing.ts
 	// extension package are outdated and may have bugs due to use old version of monaco editor API
 	const extension = new MonacoMarkdownExtension();
 	extension.activate(editorInstance as any);
