daa05c0033564453bdbb0d248e02861a5722a367
Robert Vitonsky <rob@vitonsky.net>
2023-08-12 20:41:33 +0300

    chore(#14): add import indicator

--- src/app/App/MainScreen/MainScreen/StatusBar/index.tsx
+++ src/app/App/MainScreen/MainScreen/StatusBar/index.tsx
@@ -1,5 +1,6 @@
-import React, { FC, HTMLProps } from 'react';
+import React, { FC, HTMLProps, useCallback, useState } from 'react';
 import { Button } from 'react-elegant-ui/esm/components/Button/Button.bundle/desktop';
+import { Spinner } from 'react-elegant-ui/esm/components/Spinner/Spinner.bundle/desktop';
 import { cn } from '@bem-react/classname';
 
 import { INotesRegistry } from '../../../../../core/Registry';
@@ -22,13 +23,29 @@ export const StatusBar: FC<StatusBarProps> = ({
 	updateNotes,
 	...props
 }) => {
-	const onImportNotes = useImportNotes({ notesRegistry, updateNotes, });
+	const importNotes = useImportNotes({ notesRegistry, updateNotes, });
+
+	const [isNotesImportInProgress, setIsNotesImportInProgress] = useState(false);
+	const onImportNotes = useCallback(() => {
+		setIsNotesImportInProgress(true);
+		importNotes().finally(() => {
+			setIsNotesImportInProgress(false);
+		});
+	}, [importNotes]);
 
 	return (
 		<div {...props} className={cnStatusBar({}, [className])}>
-			<Button size="s" view="action" onPress={onImportNotes}>
-				Import
-			</Button>
+			<div className={cnStatusBar('ActionContainer')}>
+				<Button size="s" view="action" onPress={onImportNotes} disabled={isNotesImportInProgress}>
+					Import
+				</Button>
+			</div>
+			<div className={cnStatusBar('StatusContainer')}>
+				{isNotesImportInProgress && <span className={cnStatusBar('ProgressIndicator')}>
+					<span>Importing notes</span>
+					<Spinner size="s" progress />
+				</span>}
+			</div>
 		</div>
 	);
 };
