31f194c5709629ff078a307eabf94390fcdced23
Robert Vitonsky <rob@vitonsky.net>
2023-08-13 18:03:21 +0300

    feat(#17): draw nested tags

--- src/app/App/MainScreen/NotesOverview/index.tsx
+++ src/app/App/MainScreen/NotesOverview/index.tsx
@@ -16,10 +16,46 @@ export const NotesOverview: FC<NotesOverviewProps> = () => {
 
 	const tagsRegistry = useTagsRegistry();
 	useEffect(() => {
-		tagsRegistry.get().then((tags) => {
-			// TODO: build tags tree
-			console.warn('tags', tags);
-			setTags(tags.map(({ id, name }) => ({ id, content: name })));
+		tagsRegistry.get().then((flatTags) => {
+			const tagsMap: Record<string, ListItem> = {};
+			const tagToParentMap: Record<string, string> = {};
+
+			// Fill maps
+			flatTags.forEach(({ id, name, parent }) => {
+				tagsMap[id] = {
+					id,
+					content: name
+				};
+
+				if (parent !== null) {
+					tagToParentMap[id] = parent;
+				}
+			});
+
+			// Attach tags to parents
+			for (const tagId in tagToParentMap) {
+				const parentId = tagToParentMap[tagId];
+
+				const tag = tagsMap[tagId];
+				const parentTag = tagsMap[parentId];
+
+				// Create array
+				if (!parentTag.childrens) {
+					parentTag.childrens = [];
+				}
+
+				// Attach tag to another tag
+				parentTag.childrens.push(tag);
+			}
+
+			// Delete nested tags from tags map
+			Object.keys(tagToParentMap).forEach((nestedTagId) => {
+				delete tagsMap[nestedTagId];
+			});
+
+			// Collect tags array from a map
+			const nestedTags = Object.values(tagsMap);
+			setTags(nestedTags);
 		});
 
 		// Run once for init state
