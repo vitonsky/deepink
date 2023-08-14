9ac8a8b29c13df9b8c182d8a17923049616a92c3
Robert Vitonsky <rob@vitonsky.net>
2023-08-15 01:18:56 +0300

    refactor(#17): add resolved names for tags

--- src/core/Registry/Tags/Tags.ts
+++ src/core/Registry/Tags/Tags.ts
@@ -2,20 +2,44 @@ import { SQLiteDb } from "../../storage/SQLiteDb";
 
 export type ITag = {
 	id: string;
+	/**
+	 * Tag name
+	 */
 	name: string;
+	/**
+	 * Tag name with full path like `foo/bar/baz`
+	 */
+	resolvedName: string;
+	/**
+	 * Id of parent tag
+	 */
 	parent: string | null;
 };
 
-/**
- * Tag with resolved name (name with nesting like `foo/bar/baz`)
- */
-export type IResolvedTag = {
-	id: string;
-	/**
-	 * Full tag name like `foo/bar/baz`
-	 */
-	name: string;
-};
+const tagsQuery = `
+WITH RECURSIVE tagTree AS (
+	SELECT
+		id, parent, name, 1 AS segmentId, id AS tagTreeId
+	FROM tags
+	UNION ALL
+	SELECT
+		t2.id, t.parent, t.name, t2.segmentId + 1 AS segmentId, t2.tagTreeId AS tagTreeId
+	FROM tags t
+	INNER JOIN tagTree t2
+	ON t.id = t2.parent
+)
+SELECT
+	t.id, t.name, x.name as resolvedName, t.parent
+FROM tags t
+INNER JOIN (
+	SELECT id, group_concat(name, "/") AS name
+	FROM (
+		SELECT * FROM tagTree ORDER BY segmentId DESC
+	)
+	GROUP BY tagTreeId
+) x
+ON t.id = x.id
+`;
 
 export class Tags {
 	private db;
@@ -26,20 +50,7 @@ export class Tags {
 	public async getTags(): Promise<ITag[]> {
 		const { db } = this.db;
 
-		const rows = await db.all('SELECT * FROM tags');
-		return rows.map(({ id, name, parent }) => ({ id, name, parent: parent ?? null }));
-	}
-
-	public async getResolvedTags(): Promise<IResolvedTag[]> {
-		const { db } = this.db;
-
-		const rows = await db.all(`WITH RECURSIVE tagTree AS (
-			SELECT id, parent, name, 1 AS segmentNum, id AS tagTreeId FROM tags
-			UNION ALL
-			SELECT t2.id, t.parent, t.name, t2.segmentNum + 1 AS segmentNum, t2.tagTreeId AS tagTreeId FROM tags t INNER JOIN tagTree t2 ON (t.id = t2.parent)
-		)
-		
-		SELECT id, group_concat(name, "/") AS name FROM (SELECT * FROM tagTree ORDER BY segmentNum DESC) GROUP BY tagTreeId`);
-		return rows;
+		const rows = await db.all(tagsQuery);
+		return rows.map(({ id, name, resolvedName, parent, }) => ({ id, name, resolvedName, parent: parent ?? null }));
 	}
 };
