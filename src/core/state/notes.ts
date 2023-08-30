de80da6135f03075f20cbccb892b35ac7ee32e59
Robert Vitonsky <rob@vitonsky.net>
2023-08-31 00:41:13 +0300

    feat: reset selected tag by remove

--- src/core/state/notes.ts
+++ src/core/state/notes.ts
@@ -1,11 +1,16 @@
 import { createApi, createEvent, createStore } from 'effector';
 
 import { INote } from '../Note';
+import { $tags } from './tags';
 
 export const $activeTag = createStore<string | null>(null);
 
 export const setActiveTag = createEvent<string | null>();
 $activeTag.on(setActiveTag, (_, activeTag) => activeTag);
+$activeTag.on($tags, (activeTag, tags) => {
+	const isActiveTagExists = tags.some(({ id }) => id === activeTag);
+	return isActiveTagExists ? activeTag : null;
+});
 
 export const $openedNotes = createStore<INote[]>([]);
 export const openedNotesControls = createApi($openedNotes, {
