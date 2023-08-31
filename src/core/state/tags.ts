aad2757b94b9928f4b6f3772b4615c1908cac941
Robert Vitonsky <rob@vitonsky.net>
2023-08-31 21:53:44 +0300

    refactor(#17): add doc comments

--- src/core/state/tags.ts
+++ src/core/state/tags.ts
@@ -2,22 +2,40 @@ import { createEvent, createStore } from "effector";
 
 import { ITag } from "../Registry/Tags/Tags";
 
+/**
+ * Array of all exists tags
+ */
 export const $tags = createStore<ITag[]>([]);
-export const tagsUpdated = createEvent<ITag[]>();
-export const tagsChanged = createEvent();
 
+/**
+ * Event to set tags list
+ */
+export const tagsUpdated = createEvent<ITag[]>();
 $tags.on(tagsUpdated, (_, payload) => payload);
 
+/**
+ * Fired after tags been changed and should be updated
+ */
+export const tagsChanged = createEvent();
+
+/**
+ * Fired after tag been attached/detached
+ */
 export const tagAttachmentsChanged = createEvent<{
 	target: string;
 	tagId: string;
 	state: 'add' | 'delete'
 }[]>();
 
+/**
+ * Id of selected tag to filter notes
+ */
 export const $activeTag = createStore<string | null>(null);
-export const setActiveTag = createEvent<string | null>();
 
+export const setActiveTag = createEvent<string | null>();
 $activeTag.on(setActiveTag, (_, activeTag) => activeTag);
+
+// Reset selected tag by remove tag
 $activeTag.on($tags, (activeTag, tags) => {
 	const isActiveTagExists = tags.some(({ id }) => id === activeTag);
 	return isActiveTagExists ? activeTag : null;
