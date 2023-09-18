7ac925265dba8d2b3aa28a78842e428080ec5824
Robert Vitonsky <rob@vitonsky.net>
2023-09-19 01:12:53 +0300

    feat(#34): introduce bytes to string converter

--- src/core/encryption/encoding.ts
+++ src/core/encryption/encoding.ts
@@ -1,3 +1,5 @@
+import { joinArrayBuffers } from './buffers';
+
 export function base64ToBytes(base64: string) {
 	const binString = atob(base64);
 	return Uint8Array.from(binString as any, (m) => (m as any).codePointAt(0)).buffer;
@@ -8,4 +10,39 @@ export function bytesToBase64(bytes: ArrayBuffer) {
 		String.fromCodePoint(x as any),
 	).join('');
 	return btoa(binString);
+}
+
+/**
+ * Convert a buffer to Base64 string with additional comma symbols
+ *
+ * Useful for a really big buffers, to avoid `RangeError: invalid array length`
+ */
+export function bytesToJoinedBase64(bytes: ArrayBuffer, sliceLen: number) {
+	const typedArray = new Uint8Array(bytes);
+	const binStrings: string[] = [];
+	for (let offset = 0; offset < typedArray.byteLength; offset += sliceLen) {
+		binStrings.push(
+			Array.from(typedArray.slice(offset, offset + sliceLen), (x) =>
+				String.fromCodePoint(x as any),
+			).join(''),
+		);
+	}
+
+	return binStrings.map((chars) => btoa(chars)).join(',');
+}
+
+/**
+ * Convert a Base64 string with additional comma symbols to buffer
+ */
+export function joinedBase64ToBytes(string: string) {
+	const binStrings = string.split(',').map((enc) => atob(enc));
+
+	const buffers = binStrings.map(
+		(binString) =>
+			Uint8Array.from(
+				binString as any as string[],
+				(m) => m.codePointAt(0) as number,
+			).buffer,
+	);
+	return joinArrayBuffers(buffers);
 }
