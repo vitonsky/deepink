f80026f4fe91d7874ca3d0ca9c15d5dd91edc7ff
Robert Vitonsky <rob@vitonsky.net>
2023-09-19 01:49:19 +0300

    chore(#34): update Rabbit cipher

--- src/core/encryption/ciphers/Rabbit/index.ts
+++ src/core/encryption/ciphers/Rabbit/index.ts
@@ -1,9 +1,35 @@
 import CryptoJS from 'crypto-js';
 
-import { base64ToBytes, bytesToBase64 } from '../../encoding';
-
 import { ICipher } from '../..';
 
+// Source https://github.com/brix/crypto-js/issues/274#issuecomment-600039187
+function CryptJsWordArrayToUint8Array(wordArray: CryptoJS.lib.WordArray) {
+	/* eslint-disable no-bitwise */
+	const l = wordArray.sigBytes;
+	const words = wordArray.words;
+	const result = new Uint8Array(l);
+	let i = 0 /*dst*/,
+		j = 0; /*src*/
+	while (true) {
+		// here i is a multiple of 4
+		if (i == l) break;
+		const w = words[j++];
+		result[i++] = (w & 0xff000000) >>> 24;
+		if (i == l) break;
+		result[i++] = (w & 0x00ff0000) >>> 16;
+		if (i == l) break;
+		result[i++] = (w & 0x0000ff00) >>> 8;
+		if (i == l) break;
+		result[i++] = w & 0x000000ff;
+	}
+	/* eslint-enable no-bitwise */
+
+	return result.buffer;
+}
+
+/**
+ * WARNING: not stable - cipher crash Chromium browser for files more than 30 Mb
+ */
 export class RabbitCipher implements ICipher {
 	private readonly key;
 	constructor(cipher: string) {
@@ -11,10 +37,11 @@ export class RabbitCipher implements ICipher {
 	}
 
 	public async encrypt(buffer: ArrayBuffer) {
-		const b64 = bytesToBase64(buffer);
-		const cipherResult = CryptoJS.Rabbit.encrypt(b64, this.key).toString(
-			CryptoJS.format.OpenSSL,
-		);
+		// Cast type, because library supports `ArrayBuffer`, but types are incorrect
+		const cipherResult = CryptoJS.Rabbit.encrypt(
+			CryptoJS.lib.WordArray.create(buffer as unknown as number[]),
+			this.key,
+		).toString(CryptoJS.format.OpenSSL);
 		return new TextEncoder().encode(cipherResult).buffer;
 	}
 
@@ -24,7 +51,6 @@ export class RabbitCipher implements ICipher {
 			CryptoJS.format.OpenSSL.parse(cipherResult),
 			this.key,
 		);
-		const b64 = wordArray.toString(CryptoJS.enc.Utf8);
-		return base64ToBytes(b64);
+		return CryptJsWordArrayToUint8Array(wordArray);
 	}
 }
