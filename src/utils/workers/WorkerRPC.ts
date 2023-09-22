2ecf8591e8ed0c32b3fb9f72a5a58adb991cd5f3
Robert Vitonsky <rob@vitonsky.net>
2023-09-22 13:54:43 +0300

    feat(#33): implement exceptions for RPC controller

--- src/utils/workers/WorkerRPC.ts
+++ src/utils/workers/WorkerRPC.ts
@@ -1,10 +1,26 @@
 import { ResponseCallback, WorkerMessenger } from './WorkerMessenger';
 
-type RequestPayload<T = any> = {
-	type: 'request' | 'response';
+type RPCMessage = {
 	channel: string;
+};
+
+type RPCRequest<T = any> = RPCMessage & {
+	type: 'request';
 	payload: T;
 };
+
+type RPCResponse<T = any> = RPCMessage & {
+	type: 'response';
+} & (
+		| {
+				status: 'ok';
+				payload?: T;
+		  }
+		| {
+				status: 'error';
+				error: any;
+		  }
+	);
 
 // TODO: implement timeout
 // TODO: reject requests with not registered handlers
@@ -15,7 +31,7 @@ export class WorkerRPC {
 	}
 
 	public sendRequest(channel: string, payload: any, transferObjects?: Transferable[]) {
-		const message: RequestPayload = {
+		const message: RPCRequest = {
 			type: 'request',
 			channel,
 			payload,
@@ -23,9 +39,13 @@ export class WorkerRPC {
 
 		return this.messenger
 			.sendRequest(message, transferObjects)
-			.then((message: RequestPayload) => {
+			.then((message: RPCResponse) => {
 				// TODO: validate data
 				if (message.type !== 'response') return;
+				if (message.status === 'error') {
+					const errorToReturn = message.error ?? new Error('Unknown error');
+					throw errorToReturn;
+				}
 
 				return message.payload;
 			});
@@ -35,40 +55,50 @@ export class WorkerRPC {
 		channel: string,
 		handler: (data: any, response: ResponseCallback) => any,
 	) {
-		const cleanup = this.messenger.onMessage(
-			async (data: RequestPayload, response) => {
-				// TODO: validate data
-				if (typeof data !== 'object' || data.channel !== channel) return;
+		const cleanup = this.messenger.onMessage(async (data: RPCRequest, response) => {
+			// TODO: validate data
+			if (typeof data !== 'object' || data.channel !== channel) return;
 
-				let isResponded = false;
-				const responseProxy: ResponseCallback = (payload, transferObjects) => {
-					const message: RequestPayload = {
-						type: 'response',
-						channel,
-						payload,
-					};
-
-					const result = response(message, transferObjects);
-
-					// Mark as responded in case function complete successful
-					isResponded = true;
-
-					return result;
+			let isResponded = false;
+			const responseProxy: ResponseCallback = (payload, transferObjects) => {
+				const message: RPCResponse = {
+					channel,
+					type: 'response',
+					status: 'ok',
+					payload,
 				};
 
-				try {
-					await handler(data.payload, responseProxy);
-					if (!isResponded) {
-						responseProxy(undefined);
-					}
-				} catch (err) {
-					if (!isResponded) {
-						console.warn('Request did not responded', { channel });
-					}
-					throw err;
+				const result = response(message, transferObjects);
+
+				// Mark as responded in case function complete successful
+				isResponded = true;
+
+				return result;
+			};
+
+			try {
+				await handler(data.payload, responseProxy);
+				if (!isResponded) {
+					responseProxy(undefined);
 				}
-			},
-		);
+			} catch (err) {
+				// Respond with error in case response still not been sent
+				if (!isResponded) {
+					console.warn('Request did not responded', { channel });
+
+					const message: RPCResponse = {
+						channel,
+						type: 'response',
+						status: 'error',
+						error: err,
+					};
+
+					response(message);
+				}
+
+				throw err;
+			}
+		});
 
 		return cleanup;
 	}
