b3eb87c7fb99125d98f0a111cbffa57bb0a526f6
Robert Vitonsky <rob@vitonsky.net>
2024-01-21 02:26:15 +0100

    feat(#10): implement ModalScreen

---
+++ src/app/App/ModalScreen/ModalScreen.tsx
@@ -0,0 +1,65 @@
+import React, { FC, HTMLAttributes, ReactNode } from 'react';
+import { Button } from 'react-elegant-ui/esm/components/Button/Button.bundle/desktop';
+import { FaXmark } from 'react-icons/fa6';
+import { cn } from '@bem-react/classname';
+
+import { Icon } from '../../components/Icon/Icon.bundle/common';
+import { Modal } from '../../components/Modal/Modal.bundle/Modal.desktop';
+
+import './ModalScreen.css';
+
+export const cnModalScreen = cn('ModalScreen');
+
+export interface ModalScreenProps extends HTMLAttributes<HTMLDivElement> {
+	isVisible?: boolean;
+	onClose?: () => void;
+	head?: ReactNode;
+	footer?: ReactNode;
+	title?: string;
+}
+
+export const ModalScreen: FC<ModalScreenProps> = ({
+	isVisible,
+	onClose,
+	title,
+	head,
+	footer,
+	children,
+	...rest
+}) => {
+	return (
+		<Modal
+			visible={isVisible}
+			onClose={onClose}
+			renderToStack
+			view="screen"
+			{...rest}
+		>
+			<div className={cnModalScreen()}>
+				{head && <div className={cnModalScreen('Head')}>{head}</div>}
+				{Boolean(title || onClose) && (
+					<div className={cnModalScreen('Head')}>
+						{title && (
+							<div className={cnModalScreen('HeadTitle')}>{title}</div>
+						)}
+						{onClose && (
+							<Button
+								view="clear"
+								className={cnModalScreen('CloseButton')}
+								onPress={onClose}
+							>
+								<Icon hasGlyph scalable boxSize="1rem">
+									<FaXmark />
+								</Icon>
+							</Button>
+						)}
+					</div>
+				)}
+
+				<div className={cnModalScreen('Body')}>{children}</div>
+
+				{footer && <div className={cnModalScreen('Footer')}>{footer}</div>}
+			</div>
+		</Modal>
+	);
+};
--- src/app/App/Preferences/Preferences.tsx
+++ src/app/App/Preferences/Preferences.tsx
@@ -3,17 +3,16 @@ import { Button } from 'react-elegant-ui/esm/components/Button/Button.bundle/des
 import { Checkbox } from 'react-elegant-ui/esm/components/Checkbox/Checkbox.bundle/desktop';
 import { Select } from 'react-elegant-ui/esm/components/Select/Select.bundle/desktop';
 import { Textinput } from 'react-elegant-ui/esm/components/Textinput/Textinput.bundle/desktop';
-import { FaWrench, FaXmark } from 'react-icons/fa6';
+import { FaWrench } from 'react-icons/fa6';
 import { cn } from '@bem-react/classname';
 
 import { Features } from '../../components/Features/Features';
 import { FeaturesHeader } from '../../components/Features/Header/FeaturesHeader';
 import { FeaturesOption } from '../../components/Features/Option/FeaturesOption';
-import { Icon } from '../../components/Icon/Icon.bundle/common';
-import { Modal } from '../../components/Modal/Modal.bundle/Modal.desktop';
 import { Stack } from '../../components/Stack/Stack';
 
 import { useBottomPanelManager } from '../MainScreen/StatusBar';
+import { ModalScreen } from '../ModalScreen/ModalScreen';
 
 import './Preferences.css';
 
@@ -43,124 +42,98 @@ export const Preferences = () => {
 	const onClose = useCallback(() => setIsOpened(false), []);
 
 	return (
-		<Modal visible={isOpened} onClose={onClose} renderToStack view="screen">
-			<div className={cnPreferences()}>
-				<div className={cnPreferences('Head')}>
-					<div className={cnPreferences('HeadStart')}></div>
-					<div className={cnPreferences('HeadEnd')}>
-						<Button
-							view="clear"
-							className={cnPreferences('CloseButton')}
-							onPress={onClose}
-						>
-							<Icon hasGlyph scalable boxSize="1rem">
-								<FaXmark />
-							</Icon>
-						</Button>
-					</div>
-				</div>
+		<ModalScreen
+			isVisible={isOpened}
+			onClose={onClose}
+			className={cnPreferences()}
+			title="Settings"
+		>
+			<div className={cnPreferences('Body')}>
+				<Features>
+					<FeaturesHeader view="primary">Database settings</FeaturesHeader>
 
-				<div className={cnPreferences('Body')}>
-					<Features>
-						<FeaturesHeader view="primary">Database settings</FeaturesHeader>
+					<FeaturesOption title="Database name">
+						<Textinput placeholder="Enter database name" />
+					</FeaturesOption>
 
-						<FeaturesOption title="Database name">
-							<Textinput placeholder="Enter database name" />
-						</FeaturesOption>
+					<FeaturesOption description="Workspaces passwords will be encrypted with master key and saved in database, to automatically open encrypted workspaces with no enter password">
+						<Checkbox label="Remember workspaces passwords" checked />
+					</FeaturesOption>
 
-						<FeaturesOption description="Workspaces passwords will be encrypted with master key and saved in database, to automatically open encrypted workspaces with no enter password">
-							<Checkbox label="Remember workspaces passwords" checked />
-						</FeaturesOption>
+					<FeaturesHeader view="section">Encryption</FeaturesHeader>
 
-						<FeaturesHeader view="section">Encryption</FeaturesHeader>
+					<FeaturesOption title="Encryption algorithm">
+						<Select
+							options={[
+								{
+									id: 'none',
+									content: 'None',
+								},
+								{
+									id: 'aes',
+									content: 'AES',
+								},
+								{
+									id: 'twofish',
+									content: 'Twofish',
+								},
+							]}
+							value="aes"
+						/>
+					</FeaturesOption>
 
-						<FeaturesOption title="Encryption algorithm">
-							<Select
-								options={[
-									{
-										id: 'none',
-										content: 'None',
-									},
-									{
-										id: 'aes',
-										content: 'AES',
-									},
-									{
-										id: 'twofish',
-										content: 'Twofish',
-									},
-								]}
-								value="aes"
-							/>
-						</FeaturesOption>
+					<FeaturesOption title="Password">
+						<Button>Update password</Button>
+					</FeaturesOption>
 
-						<FeaturesOption title="Password">
-							<Button>Update password</Button>
-						</FeaturesOption>
+					<FeaturesHeader view="section">Data</FeaturesHeader>
 
-						<FeaturesHeader view="section">Data</FeaturesHeader>
-
-						<FeaturesOption
-							title="Notes management"
-							description="You may export and import notes as markdown files with attachments. Try it if you migrate from another note taking app"
-						>
-							<Stack direction="horizontal" spacing={2}>
-								<Button>Import notes</Button>
-								<Button>Export notes</Button>
-							</Stack>
-						</FeaturesOption>
-
-						<FeaturesOption title="History">
-							<Stack direction="vertical" spacing={2}>
-								<Checkbox label="Enable history for notes" />
-								<Checkbox label="Use recycle bin" />
-							</Stack>
-						</FeaturesOption>
-
-						<FeaturesHeader view="section">Synchronization</FeaturesHeader>
-
-						<FeaturesOption description="Sync all data in database between your devices, to not loose it. All data are encrypted.">
-							<Checkbox label="Enable synchronization" checked />
-						</FeaturesOption>
-						<FeaturesOption title="Synchronization method">
-							<Select
-								options={[
-									{
-										id: 'fs',
-										content: 'File system',
-									},
-									{
-										id: 'server',
-										content: 'Server',
-									},
-								]}
-								value="fs"
-							/>
-						</FeaturesOption>
-						<FeaturesOption title="Synchronization directory">
-							<Textinput
-								placeholder="Enter path on directory"
-								value="/foo/bar"
-								disabled
-							/>
-						</FeaturesOption>
-					</Features>
-				</div>
-
-				<div className={cnPreferences('Footer')}>
-					<div className={cnPreferences('HeadStart')}></div>
-					<div className={cnPreferences('HeadEnd')}>
-						<Stack direction="horizontal">
-							<Button view="default" size="s">
-								Cancel
-							</Button>
-							<Button view="action" size="s">
-								Apply
-							</Button>
+					<FeaturesOption
+						title="Notes management"
+						description="You may export and import notes as markdown files with attachments. Try it if you migrate from another note taking app"
+					>
+						<Stack direction="horizontal" spacing={2}>
+							<Button>Import notes</Button>
+							<Button>Export notes</Button>
 						</Stack>
-					</div>
-				</div>
+					</FeaturesOption>
+
+					<FeaturesOption title="History">
+						<Stack direction="vertical" spacing={2}>
+							<Checkbox label="Enable history for notes" />
+							<Checkbox label="Use recycle bin" />
+						</Stack>
+					</FeaturesOption>
+
+					<FeaturesHeader view="section">Synchronization</FeaturesHeader>
+
+					<FeaturesOption description="Sync all data in database between your devices, to not loose it. All data are encrypted.">
+						<Checkbox label="Enable synchronization" checked />
+					</FeaturesOption>
+					<FeaturesOption title="Synchronization method">
+						<Select
+							options={[
+								{
+									id: 'fs',
+									content: 'File system',
+								},
+								{
+									id: 'server',
+									content: 'Server',
+								},
+							]}
+							value="fs"
+						/>
+					</FeaturesOption>
+					<FeaturesOption title="Synchronization directory">
+						<Textinput
+							placeholder="Enter path on directory"
+							value="/foo/bar"
+							disabled
+						/>
+					</FeaturesOption>
+				</Features>
 			</div>
-		</Modal>
+		</ModalScreen>
 	);
 };
