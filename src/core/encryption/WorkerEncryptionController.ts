102043f5ec3df5ee1242eb554be0fbda24adccf9
Robert Vitonsky <rob@vitonsky.net>
2024-01-27 18:25:16 +0100

    fix(#51): transform typed arrays

--- src/core/encryption/WorkerEncryptionController.ts
+++ src/core/encryption/WorkerEncryptionController.ts
@@ -40,6 +40,30 @@ export const convertBufferToTransferable = (
 		];
 	}
 
+	// TODO: add tests
+	// Typed arrays
+	const typedArray = [
+		Int8Array,
+		Uint8Array,
+		Uint8ClampedArray,
+		Int16Array,
+		Uint16Array,
+		Int32Array,
+		Uint32Array,
+		Float32Array,
+		Float64Array,
+		BigInt64Array,
+		BigUint64Array,
+	].find((proto) => buffer instanceof proto);
+	if (typedArray && buffer instanceof typedArray) {
+		return [
+			buffer.buffer,
+			(buffer: ArrayBuffer) => {
+				return new typedArray(buffer);
+			},
+		];
+	}
+
 	return [
 		buffer,
 		(buffer: ArrayBuffer) => {
