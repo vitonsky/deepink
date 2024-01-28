6c5020a1f55b4a630d273f48c440093a3aba0beb
Robert Vitonsky <rob@vitonsky.net>
2024-01-28 11:30:11 +0100

    test(#51): add test for Uint32Array

--- src/core/encryption/WorkerEncryptionController.test.ts
+++ src/core/encryption/WorkerEncryptionController.test.ts
@@ -1,23 +1,27 @@
 import { convertBufferToTransferable } from './WorkerEncryptionController';
 
 test('Buffer', () => {
-	const [buffer, convertBack] = convertBufferToTransferable(new Buffer(300));
+	const [buffer, convertBack] = convertBufferToTransferable(new Buffer([10, 20, 30]));
 
 	expect(buffer).toBeInstanceOf(ArrayBuffer);
 	expect(convertBack(buffer)).toBeInstanceOf(Buffer);
 });
 
 test('Uint8Array', () => {
-	const [buffer, convertBack] = convertBufferToTransferable(new Uint8Array(300));
+	const [buffer, convertBack] = convertBufferToTransferable(
+		new Uint8Array([10, 20, 30]),
+	);
 
 	expect(buffer).toBeInstanceOf(ArrayBuffer);
 	expect(convertBack(buffer)).toBeInstanceOf(Uint8Array);
 });
 
-// TODO: fix test
-test.skip('Uint32Array', () => {
-	const [buffer, convertBack] = convertBufferToTransferable(new Uint32Array([]));
+test('Uint32Array', () => {
+	const [buffer, convertBack] = convertBufferToTransferable(
+		new Uint32Array([10, 20, 30]),
+	);
 
-	expect(buffer).toBeInstanceOf(ArrayBuffer);
+	// Weird check for `ArrayBuffer` because bug in Jest: https://github.com/jestjs/jest/issues/10786
+	expect(Object.getPrototypeOf(buffer)[Symbol.toStringTag]).toBe('ArrayBuffer');
 	expect(convertBack(buffer)).toBeInstanceOf(Uint32Array);
 });
