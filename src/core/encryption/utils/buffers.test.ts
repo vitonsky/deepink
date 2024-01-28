82abb170b03ed8b9c7002d6c99413961d0e135cf
Robert Vitonsky <rob@vitonsky.net>
2024-01-28 12:15:04 +0100

    test(#51): add test for joinBuffers

--- src/core/encryption/utils/buffers.test.ts
+++ src/core/encryption/utils/buffers.test.ts
@@ -1,4 +1,4 @@
-import { convertBufferToTransferable } from './buffers';
+import { convertBufferToTransferable, joinBuffers } from './buffers';
 
 describe('convertBufferToTransferable', () => {
 	test('Buffer', () => {
@@ -27,5 +27,43 @@ describe('convertBufferToTransferable', () => {
 		// Weird check for `ArrayBuffer` because bug in Jest: https://github.com/jestjs/jest/issues/10786
 		expect(Object.getPrototypeOf(buffer)[Symbol.toStringTag]).toBe('ArrayBuffer');
 		expect(convertBack(buffer)).toBeInstanceOf(Uint32Array);
+	});
+});
+
+describe('joinBuffers', () => {
+	test('Uint8Array', () => {
+		const joinedBuffer = joinBuffers([
+			new Uint8Array([1, 2, 3]),
+			new Uint8Array([4, 5, 6]),
+		]);
+		expect(Array.from(new Uint8Array(joinedBuffer))).toEqual([1, 2, 3, 4, 5, 6]);
+	});
+
+	test('Uint32Array', () => {
+		const joinedBuffer = joinBuffers([
+			new Uint32Array([1, 2, 3]),
+			new Uint32Array([4, 5, 6]),
+		]);
+		expect(Array.from(new Uint32Array(joinedBuffer))).toEqual([1, 2, 3, 4, 5, 6]);
+	});
+
+	test('BigInt64Array', () => {
+		const b1 = new BigInt64Array(new ArrayBuffer(8));
+		b1[0] = 100n;
+
+		const b2 = new BigInt64Array(new ArrayBuffer(8));
+		b2[0] = 200n;
+
+		const joinedBuffer = joinBuffers([b1, b2]);
+		expect(Array.from(new BigInt64Array(joinedBuffer))).toEqual([100n, 200n]);
+	});
+
+	// TODO: implement support of node buffers
+	test.skip('Buffer', () => {
+		const joinedBuffer = joinBuffers([
+			Buffer.from([1, 2, 3]),
+			Buffer.from([4, 5, 6]),
+		]);
+		expect(Array.from(new Uint32Array(joinedBuffer))).toEqual([1, 2, 3, 4, 5, 6]);
 	});
 });
