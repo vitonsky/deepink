f3b7cdf7bc85c41fe35540b4d62b6f6ce76885f8
Robert Vitonsky <rob@vitonsky.net>
2024-02-14 01:27:55 +0100

    refactor: clear ipc builder code

--- src/electron/utils/ipc/index.ts
+++ src/electron/utils/ipc/index.ts
@@ -2,52 +2,53 @@ export interface ChannelOptions {
 	readonly name: string;
 }
 
-export type ApiToMappers<T extends Record<string, (...args: any[]) => Promise<any>>> = {
+type ApiSchema = Record<string, (...args: any[]) => Promise<any>>;
+
+export type ApiToMappers<T extends ApiSchema> = {
 	[K in keyof T]: (
 		rsp: T[K] extends Promise<infer R> ? R : never,
 		args: Parameters<T[K]>,
 	) => ReturnType<T[K]>;
 };
 
-export type ApiToHandlers<
-	T extends Record<string, (...args: any[]) => Promise<any>>,
-	Context = never,
-> = {
+export type ApiToHandlers<T extends ApiSchema, Context = never> = {
 	[K in keyof T]: (parameters: {
 		req: Parameters<T[K]>;
 		ctx: Context;
 	}) => ReturnType<T[K]> extends Promise<infer R> ? Promise<R | undefined> : never;
 };
 
+export type ClientFetcher = (endpoint: string, args: any[]) => Promise<any>;
+
 export type ServerRequestHandler<Context = never> = (
 	endpoint: string,
 	callback: (parameters: { req: any; ctx: Context }) => any,
 ) => () => void;
 
-export type ClientFetcher = (endpoint: string, args: any[]) => Promise<any>;
+export const createChannel = <T extends ApiSchema>(options: ChannelOptions) => {
+	const getResolvedEndpointName = (methodName: string) =>
+		[options.name, methodName].join('.');
 
-export const createChannel = <T extends Record<string, (...args: any[]) => Promise<any>>>(
-	options: ChannelOptions,
-) => {
 	return {
 		client: (fetcher: ClientFetcher, mappers: Partial<ApiToMappers<T>> = {}): T => {
+			// Return proxy object with virtual callbacks
 			return new Proxy<T>({} as any, {
 				get: function (_target, methodName: string) {
-					const endpoint = [options.name, methodName].join('.');
 					const mapper = mappers[methodName];
 					return (...args: Parameters<T[keyof T]>) =>
-						fetcher(endpoint, args).then((response) =>
-							mapper ? mapper(response, args) : response,
+						fetcher(getResolvedEndpointName(methodName), args).then(
+							(response) => (mapper ? mapper(response, args) : response),
 						);
 				},
 			});
 		},
 		server: <Context = never>(
-			requestsHandler: ServerRequestHandler<Context>,
+			receiver: ServerRequestHandler<Context>,
 			callbacks: ApiToHandlers<T, Context>,
 		): (() => void) => {
+			// Subscribe on channels
 			const cleanups = Object.entries(callbacks).map(([methodName, callback]) =>
-				requestsHandler([options.name, methodName].join('.'), callback),
+				receiver(getResolvedEndpointName(methodName), callback),
 			);
 			return () => {
 				cleanups.forEach((cleanup) => cleanup());
