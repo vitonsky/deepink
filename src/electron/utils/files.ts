5fc19aa88e11f4724b7bd9922888c23085d55e34
Robert Vitonsky <rob@vitonsky.net>
2024-02-15 02:09:46 +0100

    refactor: implement safe joinPath

--- src/electron/utils/files.ts
+++ src/electron/utils/files.ts
@@ -3,17 +3,31 @@ import path from 'path';
 
 import { isDevMode } from './app';
 
+/**
+ * Safe join path segments and resolve it
+ *
+ * In case resolved path are out of root path - thrown error
+ */
+export const joinPath = (root: string, ...segments: string[]) => {
+	const resolvedPath = path.resolve(path.join(root, ...segments));
+	if (!resolvedPath.startsWith(root)) {
+		throw new TypeError('Resolved path is out of root directory');
+	}
+
+	return resolvedPath;
+};
+
 export const getResourcesPath = (resourcePath?: string) => {
 	const rootPath = isDevMode()
-		? path.join(app.getAppPath(), 'dist')
+		? joinPath(app.getAppPath(), 'dist')
 		: process.resourcesPath;
-	return resourcePath ? path.join(rootPath, resourcePath) : rootPath;
+	return resourcePath ? joinPath(rootPath, resourcePath) : rootPath;
 };
 
 export const getUserDataPath = (resourcePath?: string) => {
 	// Docs: https://www.electronjs.org/docs/latest/api/app#appgetpathname
 	const rootPath = isDevMode()
-		? path.join(app.getAppPath(), 'tmp')
+		? joinPath(app.getAppPath(), 'tmp')
 		: app.getPath('userData');
-	return resourcePath ? path.join(rootPath, resourcePath) : rootPath;
+	return resourcePath ? joinPath(rootPath, resourcePath) : rootPath;
 };
