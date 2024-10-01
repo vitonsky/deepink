5d42a13e73a30a667369aabd20b4283843e30547
Robert Vitonsky <rob@vitonsky.net>
2024-10-01 23:21:45 +0200

    chore: allow overflow

--- src/features/NotesContainer/index.tsx
+++ src/features/NotesContainer/index.tsx
@@ -55,13 +55,7 @@ export const NotesContainer: FC<NotesContainerProps> = ({ ...props }) => {
 					onPick: noteActions.click,
 				}}
 			/>
-			<Box
-				display="flex"
-				flexGrow="100"
-				overflow="auto"
-				width="100%"
-				padding="0 .5rem"
-			>
+			<Box display="flex" flexGrow="100" width="100%" padding="0 .5rem">
 				<Notes
 					{...{
 						notes: openedNotes,
