f2fa11a712d3cb1c4416f772a43f44c81e723086
Robert Vitonsky <rob@vitonsky.net>
2024-10-15 15:11:41 +0200

    fix: use placeholder title for note with only invisible chars

--- src/core/features/notes/utils.ts
+++ src/core/features/notes/utils.ts
@@ -1,4 +1,6 @@
 import { INoteContent } from '@core/features/notes/index';
 
-export const getNoteTitle = (note: INoteContent) =>
-	(note.title || note.text).slice(0, 25) || 'Empty note';
+export const getNoteTitle = (note: INoteContent) => {
+	const title = (note.title || note.text).trim().slice(0, 25).trim();
+	return title || 'Empty note';
+};
