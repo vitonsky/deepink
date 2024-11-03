b27a381203e9d261fb946069207be288d8d38ced
Robert Vitonsky <rob@vitonsky.net>
2024-11-04 00:54:53 +0100

    chore: add dark theme

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -28,8 +28,7 @@ const scrollBarStyles = {
 	},
 };
 
-// TODO: add alternative theme
-export const theme = extendTheme({
+export const basicTheme = extendTheme({
 	styles: {
 		global: {
 			fontFamily: `-apple-system,
@@ -416,3 +415,46 @@ export const theme = extendTheme({
 		}),
 	},
 });
+
+export const darkTheme = extendTheme(basicTheme, {
+	colors: {
+		accent: {
+			// Accent color
+			100: '#e8e6ff',
+			500: '#6b00cb',
+			600: '#4e0095',
+		},
+		primary: {
+			// Primary color for controls
+			200: '#e6f0ff',
+			300: '#d7e7ff',
+			500: '#0066ff',
+			700: '#3667b5',
+		},
+		typography: {
+			primary: '#fff',
+			secondary: '#c7c7c7',
+			additional: '#ababab',
+			ghost: '#6e6e6e',
+		},
+		surface: {
+			background: '#000',
+			panel: '#323232',
+			border: '#565656',
+			alternativeBorder: '#666666',
+		},
+		dim: {
+			100: '#ffffff30',
+			400: '#ffffff26',
+		},
+		link: {
+			base: '#0066ff',
+			hover: '#0453c9',
+		},
+		overlay: {
+			500: '#ffffff75',
+		},
+	},
+});
+
+export const theme = darkTheme;
