07e845088a0945698b7d83d1675d981fbf7fb29d
Robert Vitonsky <rob@vitonsky.net>
2024-11-04 23:50:29 +0100

    chore: set styles for notifications

--- src/components/theme/basic.ts
+++ src/components/theme/basic.ts
@@ -180,6 +180,52 @@ export const basicTheme = extendTheme({
 				variant: 'default',
 			},
 		}),
+		Alert: createMultiStyleConfigHelpers([
+			'icon',
+			'container',
+		]).defineMultiStyleConfig({
+			baseStyle: {
+				icon: {
+					color: 'currentColor',
+				},
+				container: {
+					'&[data-status="info"]': {
+						backgroundColor: 'primary.200',
+						color: 'primary.500',
+					},
+				},
+			},
+		}),
+		Notifications: createMultiStyleConfigHelpers([
+			'root',
+			'head',
+			'body',
+		]).defineMultiStyleConfig({
+			baseStyle: {
+				root: {
+					position: 'absolute',
+					bottom: '2rem',
+					right: '0.5rem',
+					backgroundColor: 'surface.background',
+					border: '2px solid #eee',
+					borderRadius: '4px',
+					minWidth: '300px',
+					maxWidth: '500px',
+					maxHeight: '500px',
+					boxShadow: '0 5px 30px -20px black',
+				},
+				head: {
+					w: '100%',
+					padding: '.5rem 1rem',
+					backgroundColor: 'surface.contrastPanel',
+				},
+				body: {
+					w: '100%',
+					alignItems: 'start',
+					padding: '1rem',
+				},
+			},
+		}),
 		ModalScreen: createMultiStyleConfigHelpers([
 			'root',
 			'head',
--- src/features/MainScreen/Notifications/Notifications.tsx
+++ src/features/MainScreen/Notifications/Notifications.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { FaBell, FaXmark } from 'react-icons/fa6';
-import { Button, Card, HStack, Text, useOutsideClick, VStack } from '@chakra-ui/react';
+import {
+	Alert,
+	AlertIcon,
+	Button,
+	HStack,
+	Text,
+	useMultiStyleConfig,
+	useOutsideClick,
+	VStack,
+} from '@chakra-ui/react';
 import { useStatusBarManager } from '@features/MainScreen/StatusBar/StatusBarProvider';
 
 export const Notifications = () => {
+	const styles = useMultiStyleConfig('Notifications');
+
 	const { controls } = useStatusBarManager();
 
 	const [isVisible, setIsVisible] = useState(false);
@@ -37,39 +48,25 @@ export const Notifications = () => {
 	});
 
 	return (
-		<VStack
-			sx={{
-				display: isVisible ? undefined : 'none',
-				position: 'absolute',
-				bottom: '2rem',
-				right: '0.5rem',
-				backgroundColor: 'surface.background',
-				border: '2px solid #eee',
-				borderRadius: '4px',
-				minWidth: '300px',
-				maxWidth: '500px',
-				maxHeight: '500px',
-				boxShadow: '0 5px 30px -20px black',
-			}}
-			ref={ref}
-		>
-			<HStack w="100%" padding="1rem">
+		<VStack display={isVisible ? undefined : 'none'} sx={styles.root} ref={ref}>
+			<HStack sx={styles.head}>
 				<Text>Notifications</Text>
 				<VStack marginStart="auto">
-					<Button variant="ghost" size="sm" onClick={onClose}>
+					<Button variant="ghost" size="sm" onClick={onClose} paddingInline={0}>
 						<FaXmark />
 					</Button>
 				</VStack>
 			</HStack>
 
-			<VStack w="100%" alignItems="start" padding="1rem">
+			<VStack sx={styles.body}>
 				{[
 					'Demo message for notification',
 					'One more message in notifications container',
 				].map((message, idx) => (
-					<Card key={idx} padding="1rem" w="100%" variant="outline">
-						{message}
-					</Card>
+					<Alert key={idx} status="info" padding=".8rem" w="100%">
+						<AlertIcon />
+						<Text>{message}</Text>
+					</Alert>
 				))}
 			</VStack>
 		</VStack>
