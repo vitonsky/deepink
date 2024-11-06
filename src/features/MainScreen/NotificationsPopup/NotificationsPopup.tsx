68995c0042feecb7d71932b9a99c7cd2a7399993
Robert Vitonsky <rob@vitonsky.net>
2024-11-06 16:57:31 +0100

    feat: prevent auto close for notifications

--- src/features/MainScreen/NotificationsPopup/NotificationsPopup.tsx
+++ src/features/MainScreen/NotificationsPopup/NotificationsPopup.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaBell } from 'react-icons/fa6';
-import { useOutsideClick } from '@chakra-ui/react';
 import { Notifications } from '@components/Notifications/Notifications';
 import { useStatusBarManager } from '@features/MainScreen/StatusBar/StatusBarProvider';
 
@@ -31,15 +30,8 @@ export const NotificationsPopup = () => {
 
 	const onClose = () => setIsVisible(false);
 
-	const ref = useRef<HTMLDivElement>(null);
-	useOutsideClick({
-		ref,
-		handler: onClose,
-	});
-
 	return (
 		<Notifications
-			ref={ref}
 			isVisible={isVisible}
 			onClose={onClose}
 			title="Notifications"
