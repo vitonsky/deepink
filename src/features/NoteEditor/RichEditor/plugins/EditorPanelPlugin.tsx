c2a380e8d24c304c97928559361477e4f1b4ff65
Robert Vitonsky <rob@vitonsky.net>
2024-11-18 00:25:51 +0100

    refactor: reimplement wrapping

--- src/features/NoteEditor/RichEditor/plugins/EditorPanelPlugin.tsx
+++ src/features/NoteEditor/RichEditor/plugins/EditorPanelPlugin.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
 import {
 	$createParagraphNode,
+	$createTextNode,
+	$getRoot,
 	$getSelection,
+	$hasAncestor,
 	$isBlockElementNode,
 	$isRangeSelection,
 	CONTROLLED_TEXT_INSERTION_COMMAND,
 	ElementNode,
+	LexicalNode,
 } from 'lexical';
 import { $createCodeNode } from '@lexical/code';
 import { TOGGLE_LINK_COMMAND } from '@lexical/link';
@@ -17,12 +21,11 @@ import {
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { INSERT_HORIZONTAL_RULE_COMMAND } from '@lexical/react/LexicalHorizontalRuleNode';
 import { $createQuoteNode } from '@lexical/rich-text';
-import { $wrapNodes } from '@lexical/selection';
-import { $findMatchingParent, $wrapNodeInElement } from '@lexical/utils';
+import { $findMatchingParent } from '@lexical/utils';
 
 import { InsertingPayloadMap, useEditorPanelContext } from '../../EditorPanel';
 
-export const $insertOrWrapWithNode = (createElement: () => ElementNode) => {
+export const $wrapNodes = (createElement: (nodes: LexicalNode[]) => ElementNode) => {
 	const selection = $getSelection();
 
 	const points = selection?.getStartEndPoints();
@@ -34,7 +37,35 @@ export const $insertOrWrapWithNode = (createElement: () => ElementNode) => {
 		$isRangeSelection(selection) &&
 		(start.getNode() !== end.getNode() || start.offset !== end.offset)
 	) {
-		$wrapNodes(selection, $createParagraphNode, createElement());
+		const selectedNodes = selection.getNodes();
+		const anchorNode = selection.anchor.getNode();
+		const commonAncestor =
+			$findMatchingParent(anchorNode, (parent) =>
+				selectedNodes.every((selectedNode) => $hasAncestor(selectedNode, parent)),
+			) ?? $getRoot();
+		if (!commonAncestor || !$isBlockElementNode(commonAncestor)) return;
+
+		const firstChildNode = commonAncestor
+			.getChildren()
+			.find((children) => selectedNodes.includes(children));
+		console.log('firstChildNode', firstChildNode);
+		if (!firstChildNode) return;
+
+		const tmpNode = $createParagraphNode();
+		firstChildNode.insertBefore(tmpNode);
+
+		const topSelectedNodes = selectedNodes.filter((node) =>
+			commonAncestor.is(node.getParent()),
+		);
+
+		console.warn('FINAL STEP', {
+			commonAncestor,
+			topSelectedNodes,
+			selectedNodes,
+			parents: selectedNodes.map((node) => node.getParent()),
+		});
+		tmpNode.replace(createElement(topSelectedNodes));
+
 		return;
 	}
 
@@ -42,9 +73,12 @@ export const $insertOrWrapWithNode = (createElement: () => ElementNode) => {
 		start.getNode(),
 		(node) => $isBlockElementNode(node) && !node.isParentRequired(),
 	) as ElementNode | null;
+
 	if (!blockElement) return;
 
-	$wrapNodeInElement(blockElement, createElement);
+	const tmpNode = $createParagraphNode();
+	blockElement.replace(tmpNode);
+	tmpNode.replace(createElement([blockElement]));
 };
 
 // TODO: implement all inserting & formatting features
@@ -70,12 +104,28 @@ export const EditorPanelPlugin = () => {
 				},
 				quote() {
 					editor.update(() => {
-						$insertOrWrapWithNode($createQuoteNode);
+						$wrapNodes((nodes) => {
+							const quote = $createQuoteNode();
+							quote.append(...nodes);
+							return quote;
+						});
 					});
 				},
 				code() {
 					editor.update(() => {
-						$insertOrWrapWithNode($createCodeNode);
+						$wrapNodes((nodes) => {
+							const code = $createCodeNode();
+
+							const p = $createParagraphNode();
+							p.append(...nodes);
+							const textContent = p.getTextContent();
+							p.remove();
+
+							code.append($createTextNode(textContent));
+							code.select();
+
+							return code;
+						});
 					});
 				},
 				link({ url }) {
