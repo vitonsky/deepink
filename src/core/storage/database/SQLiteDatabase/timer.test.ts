6af9a23d4514654136a0bdb3fba64e0ebd0261de
Robert Vitonsky <rob@vitonsky.net>
2024-12-07 19:08:42 +0100

    test: debounce with deadline

---
+++ src/core/storage/database/SQLiteDatabase/timer.test.ts
@@ -0,0 +1,95 @@
+import { debounce } from 'lodash';
+
+Object.defineProperty(global, 'performance', {
+	writable: true,
+});
+jest.useFakeTimers();
+
+// The purpose of this test is to demonstrate cases and ensure correct behavior
+// of scheduler logic, since it based on proper delay implementation.
+// It does not matter which implementation for debouncing will be used,
+// it must pass all test cases.
+
+// This test does not tests "lodash" or any other third party package,
+// but tests the logic of debouncing independent of its implementation.
+describe('Sync scheduling with debounce and deadline', () => {
+	const times = {
+		deadline: 10000,
+		delay: 300,
+	};
+
+	afterEach(() => {
+		jest.clearAllTimers();
+		jest.clearAllMocks();
+	});
+
+	test('call function immediately when queue is empty', () => {
+		const callback = jest.fn();
+		const debouncedCallback = debounce(callback, times.delay, {
+			leading: true,
+			maxWait: times.deadline,
+		});
+
+		// First call
+		debouncedCallback();
+		expect(callback).toHaveBeenCalledTimes(1);
+
+		// One more call after delay time must be executed immediately
+		jest.advanceTimersByTime(times.delay);
+		debouncedCallback();
+		expect(callback).toHaveBeenCalledTimes(2);
+	});
+
+	test('delay function calls in row', () => {
+		const callback = jest.fn();
+		const debouncedCallback = debounce(callback, times.delay, {
+			leading: true,
+			maxWait: times.deadline,
+		});
+
+		// First call
+		debouncedCallback();
+		expect(callback).toHaveBeenCalledTimes(1);
+
+		// Delay next calls
+		debouncedCallback();
+		debouncedCallback();
+		debouncedCallback();
+		expect(callback).toHaveBeenCalledTimes(1);
+		jest.advanceTimersByTime(times.delay);
+		expect(callback).toHaveBeenCalledTimes(2);
+
+		// Execute call immediately, in case queue is empty
+		jest.advanceTimersByTime(times.delay);
+		debouncedCallback();
+		expect(callback).toHaveBeenCalledTimes(3);
+	});
+
+	test('call function by deadline', () => {
+		const callback = jest.fn();
+		const debouncedCallback = debounce(callback, times.delay, {
+			leading: true,
+			maxWait: times.deadline,
+		});
+
+		// First call
+		debouncedCallback();
+		expect(callback).toHaveBeenCalledTimes(1);
+
+		// Delay next calls
+		for (let i = 0; i < 100; i++) {
+			debouncedCallback();
+		}
+		expect(callback).toHaveBeenCalledTimes(1);
+
+		// Call by deadline
+		const stepTime = times.delay / 2;
+		const iterations = Math.ceil(times.deadline / stepTime);
+		for (let i = 0; i < iterations; i++) {
+			jest.advanceTimersByTime(stepTime);
+			debouncedCallback();
+		}
+
+		expect(callback).toHaveBeenCalledTimes(2);
+	});
+});
