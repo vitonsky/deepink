c2bc9d16930fca92ed780b2d4a26fc94ef338dfd
Robert Vitonsky <rob@vitonsky.net>
2024-12-13 01:50:42 +0100

    feat: implement query builder

---
+++ src/utils/sql/QueryBuilder.test.ts
@@ -0,0 +1,201 @@
+import {
+	ConditionClause,
+	GroupExpression,
+	Limit,
+	ListExpression,
+	Placeholder,
+	QueryConstrictor,
+	WhereClause,
+} from './QueryBuilder';
+
+describe('Primitives', () => {
+	test('Query constructor able to add queries one by one', () => {
+		const query = new QueryConstrictor();
+		query.add('SELECT * FROM foo WHERE foo=', query.getPlaceholder(1));
+		query.add(' LIMIT 2');
+
+		expect(query.toSQL()).toEqual({
+			sql: 'SELECT * FROM foo WHERE foo=? LIMIT 2',
+			bindings: [1],
+		});
+	});
+
+	test('Query constructors may be nested', () => {
+		const query1 = new QueryConstrictor();
+
+		const query2 = new QueryConstrictor();
+		query2.add('(SELECT id FROM bar WHERE x > ', query2.getPlaceholder(100), ')');
+		query1.add('SELECT * FROM foo WHERE foo IN ', query2);
+
+		expect(query1.toSQL()).toEqual({
+			sql: 'SELECT * FROM foo WHERE foo IN (SELECT id FROM bar WHERE x > ?)',
+			bindings: [100],
+		});
+	});
+
+	test('Query constructors may be nested with no introduce a variables', () => {
+		const query = new QueryConstrictor(
+			'SELECT * FROM foo WHERE foo IN ',
+			new QueryConstrictor(
+				'(SELECT id FROM bar WHERE x > ',
+				new Placeholder(100),
+				')',
+			),
+		);
+
+		expect(query.toSQL()).toEqual({
+			sql: 'SELECT * FROM foo WHERE foo IN (SELECT id FROM bar WHERE x > ?)',
+			bindings: [100],
+		});
+	});
+});
+
+describe('Basic clauses', () => {
+	describe('Group expressions', () => {
+		test('Empty group expression yields empty query', () => {
+			expect(new GroupExpression().toSQL()).toEqual({
+				sql: '',
+				bindings: [],
+			});
+		});
+
+		test('Nested group expression yields correct query', () => {
+			expect(
+				new GroupExpression(
+					new GroupExpression(new Placeholder(1)),
+					' OR ',
+					new GroupExpression(new Placeholder(2)),
+				).toSQL(),
+			).toEqual({
+				sql: '((?) OR (?))',
+				bindings: [1, 2],
+			});
+		});
+	});
+
+	describe('List expression', () => {
+		test('List with literals', () => {
+			expect(new ListExpression('foo', 'bar', 'baz').toSQL()).toEqual({
+				sql: '(foo,bar,baz)',
+				bindings: [],
+			});
+		});
+		test('Nested lists', () => {
+			expect(
+				new ListExpression('foo', new ListExpression('bar', 'baz')).toSQL(),
+			).toEqual({
+				sql: '(foo,(bar,baz))',
+				bindings: [],
+			});
+		});
+	});
+
+	describe('Limit expression', () => {
+		test('Limit', () => {
+			expect(new Limit({ limit: 10 }).toSQL()).toEqual({
+				sql: 'LIMIT 10',
+				bindings: [],
+			});
+		});
+		test('Offset', () => {
+			expect(new Limit({ offset: 20 }).toSQL()).toEqual({
+				sql: 'OFFSET 20',
+				bindings: [],
+			});
+		});
+		test('Limit and offset', () => {
+			expect(new Limit({ limit: 10, offset: 20 }).toSQL()).toEqual({
+				sql: 'LIMIT 10 OFFSET 20',
+				bindings: [],
+			});
+		});
+	});
+
+	describe('Condition expression', () => {
+		test('Empty condition generates empty query', () => {
+			const query = new ConditionClause();
+
+			expect(query.toSQL()).toEqual({
+				sql: '',
+				bindings: [],
+			});
+			expect(query.size()).toBe(0);
+		});
+
+		test('Single condition yields just a literal', () => {
+			expect(new ConditionClause().and('x > ', new Placeholder(0)).toSQL()).toEqual(
+				{
+					sql: 'x > ?',
+					bindings: [0],
+				},
+			);
+
+			expect(new ConditionClause().or('x > ', new Placeholder(0)).toSQL()).toEqual({
+				sql: 'x > ?',
+				bindings: [0],
+			});
+		});
+
+		test('Trivial condition expression yields sql expression', () => {
+			expect(
+				new ConditionClause()
+					.and('x > ', new Placeholder(0))
+					.or('y < ', new Placeholder(1))
+					.toSQL(),
+			).toEqual({
+				sql: 'x > ? OR y < ?',
+				bindings: [0, 1],
+			});
+		});
+
+		test('Complex condition expression consider a grouping', () => {
+			const query = new QueryConstrictor(
+				'SELECT * FROM foo WHERE ',
+				new ConditionClause()
+					.and('x > ', new Placeholder(0))
+					.or(
+						new GroupExpression(
+							new ConditionClause()
+								.and('y=', new Placeholder(1))
+								.and('z=', new Placeholder(2)),
+						),
+					),
+			);
+
+			expect(query.toSQL()).toEqual({
+				sql: 'SELECT * FROM foo WHERE x > ? OR (y=? AND z=?)',
+				bindings: [0, 1, 2],
+			});
+		});
+	});
+
+	describe('Where clause', () => {
+		test('Where clause may be filled after join', () => {
+			const where = new WhereClause();
+			const query = new QueryConstrictor('SELECT * FROM foo ', where);
+
+			// Fill where after build a query object
+			where
+				.and('x > ', new Placeholder(0))
+				.or(
+					new GroupExpression(
+						new ConditionClause()
+							.and('y=', new Placeholder(1))
+							.and('z=', new Placeholder(2)),
+					),
+				);
+
+			expect(query.toSQL()).toEqual({
+				sql: 'SELECT * FROM foo WHERE x > ? OR (y=? AND z=?)',
+				bindings: [0, 1, 2],
+			});
+		});
+
+		test('Empty where clause generates empty query', () => {
+			expect(new WhereClause().toSQL()).toEqual({
+				sql: '',
+				bindings: [],
+			});
+		});
+	});
+});
