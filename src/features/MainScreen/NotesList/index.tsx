69dfe1457b45eb664ecf417b4939cd3848e5d280
Robert Vitonsky <rob@vitonsky.net>
2024-12-31 19:18:20 +0100

    chore: prevent scroll for visible note

--- src/features/MainScreen/NotesList/index.tsx
+++ src/features/MainScreen/NotesList/index.tsx
@@ -11,6 +11,18 @@ import { useVirtualizer } from '@tanstack/react-virtual';
 
 import { useDefaultNoteContextMenu } from './NoteContextMenu/useDefaultNoteContextMenu';
 
+function isElementInViewport(node: HTMLElement) {
+	// eslint-disable-next-line spellcheck/spell-checker
+	const rect = node.getBoundingClientRect();
+
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
 export type NotesListProps = {};
 
 export const NotesList: FC<NotesListProps> = () => {
@@ -40,13 +52,18 @@ export const NotesList: FC<NotesListProps> = () => {
 	const items = virtualizer.getVirtualItems();
 
 	// Scroll to active note
+	const activeNoteRef = useRef<HTMLDivElement | null>(null);
 	useEffect(() => {
 		if (!activeNoteId) return;
+
+		// Skip if active note is in viewport
+		if (activeNoteRef.current !== null && isElementInViewport(activeNoteRef.current))
+			return;
 
 		const noteIndex = notes.findIndex((note) => note.id === activeNoteId);
 		if (noteIndex === -1) return;
 
-		virtualizer.scrollToIndex(noteIndex, { align: 'center' });
+		virtualizer.scrollToIndex(noteIndex, { align: 'start' });
 
 		// We only need scroll to active note once by its change
 		// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -93,15 +110,22 @@ export const NotesList: FC<NotesListProps> = () => {
 
 							const date = note.createdTimestamp ?? note.updatedTimestamp;
 							const text = note.content.text.slice(0, 80).trim();
+							const isActive = note.id === activeNoteId;
 
 							// TODO: get preview text from DB as prepared value
 							// TODO: show attachments
 							return (
 								<NotePreview
 									key={note.id}
-									ref={virtualizer.measureElement}
+									ref={(node) => {
+										if (isActive) {
+											activeNoteRef.current = node;
+										}
+
+										virtualizer.measureElement(node);
+									}}
 									data-index={virtualRow.index}
-									isSelected={note.id === activeNoteId}
+									isSelected={isActive}
 									title={getNoteTitle(note.content)}
 									text={text}
 									meta={
