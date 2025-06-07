495e6250391eb46865330d471c405ced06af4e4c
Irina Katsyuta <katsyuta@proton.me>
2025-06-07 21:43:06 +0200

    refactor: add context for access to event obj

--- src/features/App/Profile/index.tsx
+++ src/features/App/Profile/index.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, FC, useEffect, useMemo } from 'react';
 import { isEqual } from 'lodash';
 import { WorkspacesController } from '@core/features/workspaces/WorkspacesController';
+import { HotkeyEventsProvider } from '@features/MainScreen/HotkeyProvaider';
 import { StatusBarProvider } from '@features/MainScreen/StatusBar/StatusBarProvider';
 import { useAppDispatch, useAppSelector } from '@state/redux/hooks';
 import {
@@ -88,8 +89,10 @@ export const Profile: FC<ProfileProps> = ({ profile: currentProfile, controls })
 						value={{ profileId: profileId, workspaceId: workspace.id }}
 					>
 						<StatusBarProvider>
-							<Workspace profile={currentProfile} />
-							<ProfileStatusBar />
+							<HotkeyEventsProvider>
+								<Workspace profile={currentProfile} />
+								<ProfileStatusBar />
+							</HotkeyEventsProvider>
 						</StatusBarProvider>
 					</WorkspaceContext.Provider>
 				) : null,
