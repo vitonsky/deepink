6421cc8b161260e8183011a5eb10729fcb3a475f
Irina Katsyuta <katsyuta@proton.me>
2025-06-07 22:09:04 +0200

    chore: remove

--- src/features/App/Profile/index.tsx
+++ src/features/App/Profile/index.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, FC, useEffect, useMemo, useState } from 'react';
 import { isEqual } from 'lodash';
 import { WorkspacesController } from '@core/features/workspaces/WorkspacesController';
-import { HotkeyEventsProvider } from '@features/MainScreen/HotkeyProvaider';
+import { HotkeyEventsProvider } from '@features/App/hotkey/HotkeyProvaider';
 import { StatusBarProvider } from '@features/MainScreen/StatusBar/StatusBarProvider';
 import { useIsDeveloper } from '@hooks/useIsDeveloper';
 import { useAppDispatch, useAppSelector } from '@state/redux/hooks';
