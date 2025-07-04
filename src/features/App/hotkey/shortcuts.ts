f81a443776d8a2b71cfd1c24b42f10e10a38a14d
Irina Katsyuta <katsyuta@proton.me>
2025-07-04 17:39:46 +0200

    feat: switch opened notes

--- src/features/App/hotkey/shortcuts.ts
+++ src/features/App/hotkey/shortcuts.ts
@@ -3,6 +3,9 @@ export enum SHORTCUT_COMMANDS {
 	CLOSE_NOTE = 'closeNote',
 	RESTORE_CLOSED_NOTE = 'restoreNote',
 
+	OPEN_PREVIOUSLY_NOTE = 'previouslyNote',
+	OPEN_NEXT_NOTE = 'nextNote',
+
 	LOCK_PROFILE = 'lock Profile',
 }
 
@@ -14,6 +17,12 @@ export const shortcuts: KeyboardShortcutMap = {
 	'ctrl+n': SHORTCUT_COMMANDS.CREATE_NOTE,
 	'ctrl+w': SHORTCUT_COMMANDS.CLOSE_NOTE,
 	'ctrl+shift+t': SHORTCUT_COMMANDS.RESTORE_CLOSED_NOTE,
+
+	'ctrl+shift+tab': SHORTCUT_COMMANDS.OPEN_PREVIOUSLY_NOTE,
+	'ctrl+down': SHORTCUT_COMMANDS.OPEN_PREVIOUSLY_NOTE,
+
+	'ctrl+tab': SHORTCUT_COMMANDS.OPEN_NEXT_NOTE,
+	'ctrl+up': SHORTCUT_COMMANDS.OPEN_NEXT_NOTE,
 
 	'ctrl+l': SHORTCUT_COMMANDS.LOCK_PROFILE,
 };
