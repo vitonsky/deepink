32f8c284cf2873beca1f3d517f1917d1585f5e9a
Irina Katsyuta <katsyuta@proton.me>
2025-07-04 18:19:02 +0200

    chore: rename

--- src/features/App/hotkey/commandHooks.tsx
+++ src/features/App/hotkey/commandHooks.tsx
@@ -7,7 +7,7 @@ import { CommandEvent, useCommandEvent } from './CommandEventsProvider';
 import { SHORTCUT_COMMANDS } from './shortcuts';
 
 /**
- * Hook to execute command by name
+ * Executes a command by name
  */
 export function useCommandExecutor() {
 	const event = useCommandEvent();
@@ -18,48 +18,46 @@ export function useCommandExecutor() {
 }
 
 /**
- * Hook to bind hotkeys to commands
+ * Binds keyboard shortcuts to commands
  */
-export const useHotkeyBindings = () => {
+export const useShortcutBinding = () => {
 	const shortcuts = useSelector(selectShortcuts);
-
 	const execute = useCommandExecutor();
 
 	useEffect(() => {
-		// by default hotkeys are not enabled for INPUT SELECT TEXTAREA elements
-		// handle hotkey regardless of focused element
+		// Hotkeys are normally disabled on INPUT, SELECT, and TEXTAREA elements
+		// force handling shortcuts regardless of focus
 		hotkeys.filter = () => true;
 
-		Object.entries(shortcuts).forEach(([keyCombination, commandName]) => {
-			hotkeys(keyCombination, () => {
+		Object.entries(shortcuts).forEach(([shortcutKey, commandName]) => {
+			hotkeys(shortcutKey, () => {
 				execute(commandName);
 			});
 		});
 
 		return () => {
-			Object.keys(shortcuts).forEach((keyCombination) => {
-				hotkeys.unbind(keyCombination);
+			Object.keys(shortcuts).forEach((shortcutKey) => {
+				hotkeys.unbind(shortcutKey);
 			});
 		};
 	}, [shortcuts, execute]);
 };
 
 /**
- * Hook to subscribe to command events
+ * Subscribes to command event
  */
 export function useCommandSubscription(
-	commandName: SHORTCUT_COMMANDS,
+	command: SHORTCUT_COMMANDS,
 	callback: (data: CommandEvent) => void,
 ) {
 	const commandEvent = useCommandEvent();
 
 	useEffect(() => {
 		const unsubscribe = commandEvent.watch((data) => {
-			if (data.id !== commandName) {
-				return;
-			}
+			if (data.id !== command) return;
+
 			callback(data);
 		});
 		return () => unsubscribe();
-	}, [callback, commandEvent, commandName]);
+	}, [callback, commandEvent, command]);
 }
