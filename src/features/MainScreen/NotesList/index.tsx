8c46e16cd4aa2f37370c38c06671af324a899edd
Irina Katsyuta <katsyuta@proton.me>
2025-07-04 18:03:49 +0200

    refactor: move shortcut handling to hook

--- src/features/MainScreen/NotesList/index.tsx
+++ src/features/MainScreen/NotesList/index.tsx
@@ -2,8 +2,6 @@ import React, { FC, useEffect, useRef } from 'react';
 import { Box, Text, VStack } from '@chakra-ui/react';
 import { NotePreview } from '@components/NotePreview/NotePreview';
 import { getNoteTitle } from '@core/features/notes/utils';
-import { useCommandSubscription } from '@features/App/hotkey/commandHooks';
-import { SHORTCUT_COMMANDS } from '@features/App/hotkey/shortcuts';
 import { useNotesRegistry } from '@features/App/Workspace/WorkspaceProvider';
 import { useNoteActions } from '@hooks/notes/useNoteActions';
 import { useUpdateNotes } from '@hooks/notes/useUpdateNotes';
@@ -27,12 +25,10 @@ export const NotesList: FC<NotesListProps> = () => {
 	const updateNotes = useUpdateNotes();
 	const noteActions = useNoteActions();
 
-	const recentlyClosedNotes = useWorkspaceSelector(selectRecentlyClosedNote);
 	const activeNoteId = useWorkspaceSelector(selectActiveNoteId);
 	const notes = useWorkspaceSelector(selectNotes);
 
 	const search = useWorkspaceSelector(selectSearch);
-	const openedNotes = useWorkspaceSelector(selectOpenedNotes);
 
 	const openNoteContextMenu = useDefaultNoteContextMenu({
 		closeNote: noteActions.close,
@@ -69,36 +65,6 @@ export const NotesList: FC<NotesListProps> = () => {
 		// We only need scroll to active note once by its change
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [activeNoteId]);
-
-	useCommandSubscription(SHORTCUT_COMMANDS.CLOSE_NOTE, () => {
-		if (!activeNoteId) return;
-		noteActions.close(activeNoteId);
-	});
-	useCommandSubscription(SHORTCUT_COMMANDS.RESTORE_CLOSED_NOTE, () => {
-		if (!recentlyClosedNotes || !recentlyClosedNotes.length) return;
-		noteActions.click(recentlyClosedNotes[recentlyClosedNotes.length - 1]);
-	});
-
-	useCommandSubscription(SHORTCUT_COMMANDS.OPEN_NEXT_NOTE, () => {
-		if (openedNotes.length <= 1 || !activeNoteId) return;
-
-		const currentIndex = openedNotes.findIndex((note) => note.id === activeNoteId);
-		const isLastNote = currentIndex + 1 === openedNotes.length;
-		const nextIndex = isLastNote ? 0 : currentIndex + 1;
-
-		// If the current note is the last in the array, go back to the first
-		noteActions.click(openedNotes[nextIndex].id);
-	});
-	useCommandSubscription(SHORTCUT_COMMANDS.OPEN_PREVIOUSLY_NOTE, () => {
-		if (openedNotes.length <= 1 || !activeNoteId) return;
-
-		const currentIndex = openedNotes.findIndex((note) => note.id === activeNoteId);
-		const isFirstNote = currentIndex === 0;
-		const previouslyIndex = isFirstNote ? openedNotes.length - 1 : currentIndex - 1;
-
-		// If the current note is the first in the array, go to the last
-		noteActions.click(openedNotes[previouslyIndex].id);
-	});
 
 	// TODO: implement dragging and moving items
 	return (
