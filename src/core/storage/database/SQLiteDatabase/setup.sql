bab52bebcd3bd90a22055912806bf6b02ba3ed01
Irina Katsyuta <katsyuta@proton.me>
2025-07-09 20:51:30 +0200

    feat: add new property to notes table

--- src/core/storage/database/SQLiteDatabase/setup.sql
+++ src/core/storage/database/SQLiteDatabase/setup.sql
@@ -6,6 +6,7 @@ CREATE TABLE "notes" (
 	"text" TEXT NOT NULL,
 	"creationTime" INTEGER NOT NULL DEFAULT 0,
 	"lastUpdateTime" INTEGER NOT NULL DEFAULT 0,
+	"isDeleted" INTEGER,
 	PRIMARY KEY("id")
 );
 CREATE TABLE "files" (
