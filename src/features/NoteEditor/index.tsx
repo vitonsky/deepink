76c4ec4f1bd8fd82912cda0667aa18b71d4ecd83
Irina Katsyuta <katsyuta@proton.me>
2025-07-22 13:50:08 +0200

    refactor: parent component should conditionally renders

--- src/features/NoteEditor/index.tsx
+++ src/features/NoteEditor/index.tsx
@@ -219,7 +219,7 @@ export const Note: FC<NoteEditorProps> = memo(({ note, updateNote, updateMeta })
 			</HStack>
 
 			<HStack alignItems="center" w="100%" flexWrap="wrap">
-				{readOnlyMode ? null : (
+				{!readOnlyMode && (
 					<>
 						<HStack>
 							<Button variant="ghost" size="xs">
@@ -255,7 +255,7 @@ export const Note: FC<NoteEditorProps> = memo(({ note, updateNote, updateMeta })
 							<Text>{tag.resolvedName}</Text>
 						</HStack>
 
-						{readOnlyMode ? null : (
+						{!readOnlyMode && (
 							<Box
 								sx={{
 									'&:not(:hover)': {
@@ -296,7 +296,7 @@ export const Note: FC<NoteEditorProps> = memo(({ note, updateNote, updateMeta })
 					</Tag>
 				))}
 
-				{readOnlyMode ? null : (
+				{!readOnlyMode && (
 					<SuggestedTagsList
 						tags={notAttachedTags}
 						selectedTag={attachTagName ?? undefined}
