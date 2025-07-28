ba8b30760cf91067ab2b80ea9066d6d48dc45afd
Irina Katsyuta <katsyuta@proton.me>
2025-07-28 23:29:40 +0200

    refactor: add map for store algorithm names

--- src/core/features/encryption/algorithms.ts
+++ src/core/features/encryption/algorithms.ts
@@ -4,3 +4,11 @@ export enum ENCRYPTION_ALGORITHM {
 	AES_TWOFISH = 'AES-Twofish',
 	TWOFISH_AES = 'Twofish-AES',
 }
+
+// algorithm names displayed to the user
+export const ENCRYPTION_ALGORITHM_NAMES = {
+	[ENCRYPTION_ALGORITHM.AES]: 'AES',
+	[ENCRYPTION_ALGORITHM.TWOFISH]: 'Twofish',
+	[ENCRYPTION_ALGORITHM.AES_TWOFISH]: 'AES - Twofish',
+	[ENCRYPTION_ALGORITHM.TWOFISH_AES]: 'Twofish - AES',
+};
