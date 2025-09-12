20a14909b445ff4f798584bbfc3f5465c13b29bf
Irina Katsyuta <katsyuta@proton.me>
2025-09-12 14:15:08 +0200

    chore: move

--- src/core/features/commands/shortcuts/index.ts
+++ src/core/features/commands/shortcuts/index.ts
@@ -1,34 +1,4 @@
-export enum GLOBAL_COMMANDS {
-	/**
-	 * Create and open a new note
-	 */
-	CREATE_NOTE = 'createNote',
-
-	/**
-	 * Close the current note
-	 */
-	CLOSE_NOTE = 'closeNote',
-
-	/**
-	 * Reopen the last closed note
-	 */
-	RESTORE_CLOSED_NOTE = 'restoreNote',
-
-	/**
-	 * Move to the note on the left
-	 */
-	NAVIGATE_NOTE_LEFT = 'openPreviouslyNote',
-
-	/**
-	 * Move to the note on the right
-	 */
-	NAVIGATE_NOTE_RIGHT = 'openNextNote',
-
-	/**
-	 * Lock the user profile
-	 */
-	LOCK_PROFILE = 'lockProfile',
-}
+import { GLOBAL_COMMANDS } from '..';
 
 export type KeyboardShortcutMap = {
 	[key: string]: GLOBAL_COMMANDS;
