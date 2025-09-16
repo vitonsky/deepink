9de1816747f76b77d68dba4c24355c3dde95a106
Irina Katsyuta <katsyuta@proton.me>
2025-09-16 19:35:47 +0200

    refactor: reuse code

--- src/hooks/notes/useNoteShortcutActions.ts
+++ src/hooks/notes/useNoteShortcutActions.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { GLOBAL_COMMANDS } from '@core/features/commands';
 import { useCommandListener } from '@core/features/commands/useCommandListener';
+import { INote } from '@core/features/notes';
 import { useCreateNote } from '@hooks/notes/useCreateNote';
 import { useNoteActions } from '@hooks/notes/useNoteActions';
 import { useWorkspaceSelector } from '@state/redux/profiles/hooks';
@@ -9,6 +10,24 @@ import {
 	selectOpenedNotes,
 	selectRecentlyClosedNotes,
 } from '@state/redux/profiles/profiles';
+
+/**
+ * When reaching the end of the array, returns the first element, when reaching the start of the array, returns the last element
+ */
+const getRelativeNoteId = (
+	openedNotes: INote[],
+	activeNoteId: string | null,
+	offset: number,
+) => {
+	// switch only if 2 or more notes are opened
+	if (!activeNoteId || openedNotes.length < 2) return null;
+
+	const currentIndex = openedNotes.findIndex((note) => note.id === activeNoteId);
+	if (currentIndex === -1) return null;
+
+	const nextIndex = (currentIndex + offset + openedNotes.length) % openedNotes.length;
+	return openedNotes[nextIndex].id;
+};
 
 /**
  * Hook handles note actions triggered via keyboard shortcuts, including create, close, restore, and navigation
@@ -42,36 +61,18 @@ export const useNoteShortcutActions = () => {
 	useCommandListener(
 		GLOBAL_COMMANDS.FOCUS_PREVIOUS_NOTE,
 		useCallback(() => {
-			// switch only if 2 or more notes are opened
-			if (!activeNoteId || openedNotes.length < 2) return;
-
-			const currentIndex = openedNotes.findIndex(
-				(note) => note.id === activeNoteId,
-			);
-			const isLastNote = currentIndex + 1 === openedNotes.length;
-
-			// If the current note is the last in the array, go back to the first note
-			const nextIndex = isLastNote ? 0 : currentIndex + 1;
-			noteActions.click(openedNotes[nextIndex].id);
+			const previousNote = getRelativeNoteId(openedNotes, activeNoteId, +1);
+			if (!previousNote) return;
+			noteActions.click(previousNote);
 		}, [activeNoteId, noteActions, openedNotes]),
 	);
 
 	useCommandListener(
 		GLOBAL_COMMANDS.FOCUS_NEXT_NOTE,
 		useCallback(() => {
-			// switch only if 2 or more notes are opened
-			if (!activeNoteId || openedNotes.length < 2) return;
-
-			const currentIndex = openedNotes.findIndex(
-				(note) => note.id === activeNoteId,
-			);
-			const isFirstNote = currentIndex === 0;
-
-			// If the current note is the first in the array, go to the last note
-			const previouslyIndex = isFirstNote
-				? openedNotes.length - 1
-				: currentIndex - 1;
-			noteActions.click(openedNotes[previouslyIndex].id);
+			const nextNote = getRelativeNoteId(openedNotes, activeNoteId, -1);
+			if (!nextNote) return;
+			noteActions.click(nextNote);
 		}, [activeNoteId, noteActions, openedNotes]),
 	);
 };
