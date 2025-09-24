be73718eed04aba8c722e91e0b0ed942de52dd2e
Robert Vitonsky <robert@vitonsky.net>
2025-09-24 19:08:20 +0200

    test: remove environment config

--- src/core/storage/interop/import/NotesImporter.test.ts
+++ src/core/storage/interop/import/NotesImporter.test.ts
@@ -1,4 +1,3 @@
-// @vitest-environment jsdom
 import { AttachmentsController } from '@core/features/attachments/AttachmentsController';
 import { createFileManagerMock } from '@core/features/files/__tests__/mocks/createFileManagerMock';
 import { FilesController } from '@core/features/files/FilesController';
