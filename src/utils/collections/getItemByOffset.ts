54e1dbc551a1c614bcb31cad886c0609982c9ff8
Irina Katsyuta <katsyuta@proton.me>
2025-09-26 15:17:23 +0200

    refactor: improve utils, test

--- src/utils/collections/getItemByOffset.ts
+++ src/utils/collections/getItemByOffset.ts
@@ -4,9 +4,12 @@
  * Returns the first item when going past the end, and the last item when going before the start
  */
 export const getItemByOffset = <T>(collection: T[], index: number, offset: number) => {
-	if (collection.length < 2) return null;
+	if (collection.length <= 2 || index >= collection.length) return null;
 
-	const nextIndex =
-		(((index + offset) % collection.length) + collection.length) % collection.length;
+	// for handle large negative offsets, we need to use the module formula, otherwise we would get a negative index
+	// formula ensures that the result is always in the range [0, collection.length - 1]
+	const mathModule = (n: number, m: number) => ((n % m) + m) % m;
+
+	const nextIndex = mathModule(index + offset, collection.length);
 	return collection[nextIndex];
 };
