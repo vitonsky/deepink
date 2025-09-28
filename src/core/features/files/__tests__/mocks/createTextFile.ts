bb00be672ec339cf2d36e2548d7456c903b7a4af
Robert Vitonsky <robert@vitonsky.net>
2025-09-29 01:13:50 +0200

    chore: update util to make text file for tests

--- src/core/features/files/__tests__/mocks/createTextFile.ts
+++ src/core/features/files/__tests__/mocks/createTextFile.ts
@@ -1,4 +1,4 @@
-export const File = require('blob-polyfill').File;
+require('blob-polyfill');
 
 export const createTextFile = (text: string): File =>
-	new File([Buffer.from(text).buffer], 'test.txt', { type: 'text/txt' });
+	new File([text], 'test.txt', { type: 'text/plain' });
