2d70310a43ca5d722d00eaafd1c9c5ca51dfb1e8
Irina Katsyuta <katsyuta@proton.me>
2025-10-06 20:32:18 +0200

    chore: fix typo

--- src/utils/collections/getItemByOffset.test.ts
+++ src/utils/collections/getItemByOffset.test.ts
@@ -2,7 +2,7 @@ import { getItemByOffset } from '@utils/collections/getItemByOffset';
 
 const collection = Array.from({ length: 10 }, (_, i) => `item${i}`);
 
-test('Do not navigate when collection has fewer than 2 items, returning null', () => {
+test('Returns null if the collection has fewer than 2 items', () => {
 	expect(getItemByOffset([], 0, 1)).toBe(null);
 	expect(getItemByOffset([1], 0, 1)).toBe(null);
 	expect(getItemByOffset([1, 2], 0, 1)).toBe(2);
@@ -12,58 +12,58 @@ test('Returns null for an index outside the collection', () => {
 });
 
 describe('Receive next item (positive offset)', () => {
-	test('Navigate to next from the first item with offset 1, receiving the second item', () => {
+	test('Navigates to the next item from the 1st, receiving the 2nd item', () => {
 		expect(getItemByOffset(collection, 0, 1)).toBe(collection[1]);
 	});
-	test('Navigate to next from the second item with offset 1, receiving the 3rd item', () => {
+	test('Navigates to the next item from the 2nd, receiving the 3rd item', () => {
 		expect(getItemByOffset(collection, 1, 1)).toBe(collection[2]);
 	});
-	test('Navigate to next from the third item with offset 1, receiving the 4th item', () => {
+	test('Navigates to the next item from the 3rd, receiving the 4th item', () => {
 		expect(getItemByOffset(collection, 2, 1)).toBe(collection[3]);
 	});
-	test('Navigate to next from the sixth item with offset 1, receiving the 7th item', () => {
+	test('Navigates to the next item from the 6th, receiving the 7th item', () => {
 		expect(getItemByOffset(collection, 5, 1)).toBe(collection[6]);
 	});
 });
 
 describe('Receive previous item (negative offset)', () => {
-	test('Navigate to previous from the 10th item with offset -1, receiving the 9th item', () => {
+	test('Navigates to the previous item from the 10th, receiving the 9th item', () => {
 		expect(getItemByOffset(collection, 9, -1)).toBe(collection[8]);
 	});
-	test('Navigate to previous from the 9th item with offset -1, receiving the 8th item', () => {
+	test('Navigates to the previous item from the 9th, receiving the 8th item', () => {
 		expect(getItemByOffset(collection, 8, -1)).toBe(collection[7]);
 	});
-	test('Navigate to previous from the 6th item with offset -1, receiving the 5th item', () => {
+	test('Navigates to the previous item from the 6th, receiving the 5th item', () => {
 		expect(getItemByOffset(collection, 5, -1)).toBe(collection[4]);
 	});
 });
 
 describe('Circular navigation', () => {
-	test('Navigate forward from end to start with offset 1, wrapping to the first item', () => {
+	test('Navigates forward from the last to the first with offset 1, wrapping to the 1st item of 10', () => {
 		expect(getItemByOffset(collection, 9, 1)).toBe(collection[0]);
 	});
-	test('Navigate forward from end to start with offset 3, wrapping to the 3rd item', () => {
+	test('Navigates forward from the last item with offset 3, wrapping to the 3rd item of 10', () => {
 		expect(getItemByOffset(collection, 9, 3)).toBe(collection[2]);
 	});
-	test('Navigate backward from start to end with offset -1, wrapping to the last item', () => {
+	test('Navigates backward from the first to the last with offset -1, wrapping to the 10th item of 10', () => {
 		expect(getItemByOffset(collection, 0, -1)).toBe(collection[9]);
 	});
-	test('Navigate backward from start to end with offset -3, wrapping to the 8th item', () => {
+	test('Navigates backward from the first item with offset -3, wrapping to the 8th item of 10', () => {
 		expect(getItemByOffset(collection, 0, -3)).toBe(collection[7]);
 	});
 });
 
 describe('Circular navigation with large offset', () => {
-	test('Navigate from the 3rd item to the next with offset 73, wrapping to the 6th item', () => {
+	test('Navigates forward from the 3rd item with offset 73, wrapping to the 6th item of 10', () => {
 		expect(getItemByOffset(collection, 2, 73)).toBe(collection[5]);
 	});
-	test('Navigate backward from the 3rd item with offset -73, wrapping to the last item', () => {
+	test('Navigates backward from the 3rd item with offset -73, wrapping to the 10th item of 10', () => {
 		expect(getItemByOffset(collection, 2, -73)).toBe(collection[9]);
 	});
-	test('Navigate forward from the 3rd item with offset 1004, wrapping to the 7th item', () => {
+	test('Navigates forward from the 3rd item with offset 1004, wrapping to the 7th item of 10', () => {
 		expect(getItemByOffset(collection, 2, 1004)).toBe(collection[6]);
 	});
-	test('Navigate backward from the 3rd item with offset -1004, wrapping to the 9th item', () => {
+	test('Navigates backward from the 3rd item with offset -1004, wrapping to the 9th item of 10', () => {
 		expect(getItemByOffset(collection, 2, -1004)).toBe(collection[8]);
 	});
 });
