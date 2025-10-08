e1546fa5116de9d578826d922e3afeb017d5e925
Robert Vitonsky <robert@vitonsky.net>
2025-10-08 09:44:36 +0200

    test: organize test file

--- src/core/storage/database/pglite/PGLiteDatabase.test.ts
+++ src/core/storage/database/pglite/PGLiteDatabase.test.ts
@@ -2,34 +2,36 @@ import { createFileControllerMock } from '@utils/mocks/fileControllerMock';
 
 import { openDatabase } from './PGLiteDatabase';
 
-const file = createFileControllerMock();
+describe('Database persistence', () => {
+	const file = createFileControllerMock();
 
-test('Init DB and close', async () => {
-	const dbContainer = await openDatabase(file);
-	const db = dbContainer.get();
+	test('Database must dump its content while closing', async () => {
+		const dbContainer = await openDatabase(file);
+		const db = dbContainer.get();
 
-	await db.query(
-		`CREATE TABLE test (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), text TEXT NOT NULL)`,
-	);
-	await db.query(`INSERT INTO test(text) VALUES ('Hello world')`);
+		await db.query(
+			`CREATE TABLE test (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), text TEXT NOT NULL)`,
+		);
+		await db.query(`INSERT INTO test(text) VALUES ('Hello world')`);
 
-	await dbContainer.close();
-});
+		await dbContainer.close();
+	});
 
-test('Load DB with all data', async () => {
-	const dbContainer = await openDatabase(file);
-	const db = dbContainer.get();
+	test('Database must restore its data while initialization', async () => {
+		const dbContainer = await openDatabase(file);
+		const db = dbContainer.get();
 
-	await expect(db.query('SELECT * FROM test;')).resolves.toEqual(
-		expect.objectContaining({
-			rows: [
-				{
-					id: expect.any(String),
-					text: 'Hello world',
-				},
-			],
-		}),
-	);
+		await expect(db.query('SELECT * FROM test;')).resolves.toEqual(
+			expect.objectContaining({
+				rows: [
+					{
+						id: expect.any(String),
+						text: 'Hello world',
+					},
+				],
+			}),
+		);
 
-	await dbContainer.close();
+		await dbContainer.close();
+	});
 });
