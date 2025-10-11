b5d9630ff94363bdb35f2a153f822af7a435a8ab
Irina Katsyuta <katsyuta@proton.me>
2025-10-12 00:01:33 +0200

    chore: rename

--- src/features/App/Profiles/index.tsx
+++ src/features/App/Profiles/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { LexemesRegistry } from '@core/features/notes/controller/LexemesRegistry';
-import { CommandBusProvider } from '@hooks/commands/CommandEventProvider';
+import { CommandProvider } from '@hooks/commands/CommandProvider';
 import { useAppDispatch } from '@state/redux/hooks';
 import { workspacesApi } from '@state/redux/profiles/profiles';
 
@@ -43,9 +43,9 @@ export const Profiles: FC<ProfilesProps> = ({ profilesApi }) => {
 						value={controls}
 						key={profile.profile.id}
 					>
-						<CommandBusProvider>
+						<CommandProvider>
 							<Profile profile={profile} controls={controls} />
-						</CommandBusProvider>
+						</CommandProvider>
 					</ProfileControlsContext.Provider>
 				);
 			})}
--- src/hooks/commands/useWorkspaceCommandCallback.ts
+++ src/hooks/commands/useWorkspaceCommandCallback.ts
@@ -5,22 +5,24 @@ import { useWorkspaceData } from '@state/redux/profiles/hooks';
 import { selectActiveWorkspaceInfo } from '@state/redux/profiles/profiles';
 
 import { useCommandCallback } from './useCommandCallback';
-import { CommandPayloads } from '.';
+import { CommandPayloadsMap } from '.';
 
 /**
  * Subscribes to a command event only within the current workspace context
  * and automatically unsubscribes when the workspace is not active or the component unmounts.
  */
-export const useWorkspaceCommandCallback = <K extends keyof CommandPayloads>(
+export const useWorkspaceCommandCallback = <K extends keyof CommandPayloadsMap>(
 	commandName: K,
-	callback: (payload: CommandPayloads[K]) => void,
+	callback: (payload: CommandPayloadsMap[K]) => void,
+	options?: { enabled?: boolean },
 ) => {
 	const { profileId, workspaceId: contextWorkspaceId } = useWorkspaceData();
 	const activeWorkspace = useAppSelector(
 		useMemo(() => selectActiveWorkspaceInfo({ profileId }), [profileId]),
 		isEqual,
 	);
+
 	const isEnabled = activeWorkspace?.id === contextWorkspaceId;
 
-	useCommandCallback(commandName, callback, { enabled: isEnabled });
+	useCommandCallback(commandName, callback, { enabled: isEnabled ?? options?.enabled });
 };
