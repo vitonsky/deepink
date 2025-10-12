6e043ab30d0eae9c6961b897c4f4f273417e6a49
Irina Katsyuta <katsyuta@proton.me>
2025-10-12 12:16:28 +0200

    refactor: rename, improve

--- src/hooks/commands/useCommand.ts
+++ src/hooks/commands/useCommand.ts
@@ -1,28 +1,31 @@
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 
-import { CommandContext } from './CommandProvider';
+import { CommandEventContext } from './CommandEventProvider';
 import { CommandPayloadsMap } from '.';
 
 /**
  * Provides a function that triggers a command event
  */
 export const useCommand = () => {
-	const commandEvent = useContext(CommandContext);
+	const commandEvent = useContext(CommandEventContext);
 
-	return <K extends keyof CommandPayloadsMap>(
-		commandName: K,
-		...args: CommandPayloadsMap[K] extends void
-			? [payload?: undefined]
-			: [payload: CommandPayloadsMap[K]]
-	) => {
-		const [payload] = args;
-		const data =
-			payload === undefined
-				? { name: commandName }
-				: {
-						name: commandName,
-						payload,
-				  };
-		commandEvent(data);
-	};
+	return useCallback(
+		<K extends keyof CommandPayloadsMap>(
+			commandName: K,
+			...args: CommandPayloadsMap[K] extends void
+				? [payload?: undefined]
+				: [payload: CommandPayloadsMap[K]]
+		) => {
+			const [payload] = args;
+			const data =
+				payload === undefined
+					? { name: commandName }
+					: {
+							name: commandName,
+							payload,
+					  };
+			commandEvent(data);
+		},
+		[commandEvent],
+	);
 };
