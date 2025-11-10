79ca075f78835113ee3b1d7a7df6b9b72764943c
Irina Katsyuta <katsyuta@proton.me>
2025-11-10 22:03:42 +0100

    chore: improve returning

--- src/state/redux/profiles/selectors/view.ts
+++ src/state/redux/profiles/selectors/view.ts
@@ -4,6 +4,8 @@ import { createWorkspaceSelector, selectWorkspaceRoot } from '../utils';
 export const selectNotesView = createWorkspaceSelector(
 	[selectWorkspaceRoot],
 	(workspace) => {
-		return workspace?.view ?? NOTES_VIEW.All_NOTES;
+		if (!workspace) return NOTES_VIEW.All_NOTES;
+
+		return workspace.view;
 	},
 );
