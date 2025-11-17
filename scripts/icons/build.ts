04064a3d7d7c8849cddd2bf4f1f021a015c85fec
Vitonsky <rob@vitonsky.net>
2025-11-17 01:49:13 +0100

    feat: use simple icon in tray

--- scripts/icons/build.ts
+++ scripts/icons/build.ts
@@ -6,7 +6,11 @@ async function buildIcons() {
 	const iconsDir = path.resolve('./assets/icons');
 	const sourceIcon = path.join(iconsDir, 'app.svg');
 
-	const tasks: Promise<any>[] = [buildPng(sourceIcon), buildIco(sourceIcon)];
+	const tasks: Promise<any>[] = [
+		buildPng(path.join(iconsDir, 'icon-simple.svg')),
+		buildPng(sourceIcon),
+		buildIco(sourceIcon),
+	];
 
 	// Build only if CLI tools available
 	if (process.platform === 'darwin') {
