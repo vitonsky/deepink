1661d2bbd5f10b8be09bab828b13e6aa14b5ed9c
Irina Katsyuta <katsyuta@proton.me>
2025-12-06 23:38:33 +0100

    test: add

--- src/core/features/notes/controller/BookmarksController.test.ts
+++ src/core/features/notes/controller/BookmarksController.test.ts
@@ -44,3 +44,25 @@ test('bookmark is removed when note is deleted', async () => {
 		expect.arrayContaining([]),
 	);
 });
+
+test('getList returns correct bookmarks after delete', async () => {
+	const dbFile = createFileControllerMock();
+	const db = await openDatabase(dbFile);
+	const noteRegistry = new NotesController(db, FAKE_WORKSPACE_ID);
+	const bookmarksRegistry = new BookmarksController(db, FAKE_WORKSPACE_ID);
+
+	const notes = [
+		{ title: 'Note 1', text: 'Text 1' },
+		{ title: 'Note 2', text: 'Text 2' },
+		{ title: 'Note 3', text: 'Text 3' },
+	];
+	const noteIds = await Promise.all(notes.map((n) => noteRegistry.add(n)));
+	await Promise.all(noteIds.map((n) => bookmarksRegistry.add(n)));
+	await expect(bookmarksRegistry.getList()).resolves.toHaveLength(3);
+
+	await bookmarksRegistry.delete([noteIds[0]]);
+	await expect(bookmarksRegistry.getList()).resolves.toHaveLength(2);
+
+	await bookmarksRegistry.delete(noteIds);
+	await expect(bookmarksRegistry.getList()).resolves.toHaveLength(0);
+});
