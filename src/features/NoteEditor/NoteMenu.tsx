991492402cede63f2a49272ef9bc2702d9bab932
Irina Katsyuta <katsyuta@proton.me>
2026-01-04 21:39:58 +0100

    feat: add command for history

--- src/features/NoteEditor/NoteMenu.tsx
+++ src/features/NoteEditor/NoteMenu.tsx
@@ -24,102 +24,96 @@ import {
 	Text,
 } from '@chakra-ui/react';
 import { INote } from '@core/features/notes';
-
-import { NoteSidebarTabs } from '.';
+import { GLOBAL_COMMANDS } from '@hooks/commands';
+import { useCommand } from '@hooks/commands/useCommand';
 
 // TODO: call commands by click items
-export const NoteMenu = memo(
-	({
-		note,
-		onClick,
-	}: {
-		note: INote;
-		onClick?: (command: NoteSidebarTabs) => void;
-	}) => {
-		return (
-			<Menu>
-				<MenuButton as={Button} variant="ghost" size="sm">
-					<FaEllipsis />
-				</MenuButton>
-				<MenuList>
-					<MenuItem>
-						<HStack>
-							<FaCopy />
-							<Text>Copy reference on note</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaBell />
-							<Text>Remind me</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem onClick={() => onClick?.(NoteSidebarTabs.HISTORY)}>
-						<HStack>
-							<FaClock />
-							<Text>History</Text>
-							{note.isSnapshotsDisabled && (
-								<Text color="typography.secondary">(Disabled)</Text>
-							)}
-						</HStack>
-					</MenuItem>
-					<MenuItem onClick={() => onClick?.(NoteSidebarTabs.BACK_LINKS)}>
-						<HStack>
-							<FaLink />
-							<Text>Back links</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaEye />
-							<Text>Readonly mode</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaDownload />
-							<Text>Download and convert a network media</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaSpellCheck />
-							<Text>Spellcheck</Text>
-						</HStack>
-					</MenuItem>
+export const NoteMenu = memo(({ note }: { note: INote }) => {
+	const runCommand = useCommand();
 
-					<MenuItem>
-						<HStack>
-							<FaFileExport />
-							<Text>Export...</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaShield />
-							<Text>Password protection...</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaRotate />
-							<Text>Disable sync</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaBoxArchive />
-							<Text>Archive</Text>
-						</HStack>
-					</MenuItem>
-					<MenuItem>
-						<HStack>
-							<FaTrashCan />
-							<Text>Delete</Text>
-						</HStack>
-					</MenuItem>
-				</MenuList>
-			</Menu>
-		);
-	},
-);
+	return (
+		<Menu>
+			<MenuButton as={Button} variant="ghost" size="sm">
+				<FaEllipsis />
+			</MenuButton>
+			<MenuList>
+				<MenuItem>
+					<HStack>
+						<FaCopy />
+						<Text>Copy reference on note</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaBell />
+						<Text>Remind me</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem onClick={() => runCommand(GLOBAL_COMMANDS.OPEN_NOTE_HISTORY)}>
+					<HStack>
+						<FaClock />
+						<Text>History</Text>
+						{note.isSnapshotsDisabled && (
+							<Text color="typography.secondary">(Disabled)</Text>
+						)}
+					</HStack>
+				</MenuItem>
+				<MenuItem onClick={() => console.log('TODO: show note backlinks')}>
+					<HStack>
+						<FaLink />
+						<Text>Back links</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaEye />
+						<Text>Readonly mode</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaDownload />
+						<Text>Download and convert a network media</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaSpellCheck />
+						<Text>Spellcheck</Text>
+					</HStack>
+				</MenuItem>
+
+				<MenuItem>
+					<HStack>
+						<FaFileExport />
+						<Text>Export...</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaShield />
+						<Text>Password protection...</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaRotate />
+						<Text>Disable sync</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaBoxArchive />
+						<Text>Archive</Text>
+					</HStack>
+				</MenuItem>
+				<MenuItem>
+					<HStack>
+						<FaTrashCan />
+						<Text>Delete</Text>
+					</HStack>
+				</MenuItem>
+			</MenuList>
+		</Menu>
+	);
+});
