dbfb0adf2dc8100d7936aff108ad58ecc99b9bab
Irina Katsyuta <katsyuta@proton.me>
2026-01-05 20:31:19 +0100

    refactor: move, use command for export

--- src/features/NotesContainer/NoteContextMenu/useNoteContextMenu.ts
+++ src/features/NotesContainer/NoteContextMenu/useNoteContextMenu.ts
@@ -4,23 +4,14 @@ import { INote, NoteId } from '@core/features/notes';
 import { TELEMETRY_EVENT_NAME } from '@core/features/telemetry';
 import { ContextMenu } from '@electron/requests/contextMenu';
 import {
-	useNotesContext,
 	useNotesRegistry,
 	useTagsRegistry,
 } from '@features/App/Workspace/WorkspaceProvider';
 import { useTelemetryTracker } from '@features/telemetry';
 import { GLOBAL_COMMANDS } from '@hooks/commands';
 import { useCommand } from '@hooks/commands/useCommand';
-import { useWorkspaceCommandCallback } from '@hooks/commands/useWorkspaceCommandCallback';
-import { useNoteActions } from '@hooks/notes/useNoteActions';
-import { buildFileName, useNotesExport } from '@hooks/notes/useNotesExport';
-import { useUpdateNotes } from '@hooks/notes/useUpdateNotes';
 import { ContextMenuCallback } from '@hooks/useContextMenu';
 import { useShowNoteContextMenu } from '@hooks/useShowNoteContextMenu';
-import { useAppSelector } from '@state/redux/hooks';
-import { useWorkspaceData } from '@state/redux/profiles/hooks';
-import { selectWorkspace } from '@state/redux/profiles/profiles';
-import { selectConfirmMoveToBin } from '@state/redux/settings/selectors/preferences';
 import { copyTextToClipboard } from '@utils/clipboard';
 
 import { NoteActions } from '.';
@@ -60,75 +51,11 @@ const buildNoteMenu = (note: INote): ContextMenu => {
 	];
 };
 
-export const useHandleDeleteCommand = () => {
-	const telemetry = useTelemetryTracker();
-	const notes = useNotesRegistry();
-	const tagsRegistry = useTagsRegistry();
-
-	const { noteUpdated } = useNotesContext();
-	const updateNotes = useUpdateNotes();
-	const noteActions = useNoteActions();
-
-	const requiresConfirmMoveToBin = useAppSelector(selectConfirmMoveToBin);
-
-	useWorkspaceCommandCallback(GLOBAL_COMMANDS.DELETE_NOTE_TO_BIN, async ({ id }) => {
-		if (requiresConfirmMoveToBin) {
-			const isConfirmed = confirm(`Do you want to move this note to the bin?`);
-			if (!isConfirmed) return;
-		}
-
-		noteActions.close(id);
-
-		await notes.updateMeta([id], { isDeleted: true });
-		const updatedNote = await notes.getById(id);
-		if (updatedNote) noteUpdated(updatedNote);
-
-		updateNotes();
-
-		telemetry.track(TELEMETRY_EVENT_NAME.NOTE_DELETED, {
-			permanently: 'false',
-		});
-	});
-
-	useWorkspaceCommandCallback(
-		GLOBAL_COMMANDS.DELETE_NOTE_PERMANENTLY,
-		async ({ id }) => {
-			const isConfirmed = confirm(`Do you want to permanently delete this note?`);
-			if (!isConfirmed) return;
-
-			noteActions.close(id);
-
-			await notes.delete([id]);
-			await tagsRegistry.setAttachedTags(id, []);
-
-			updateNotes();
-
-			telemetry.track(TELEMETRY_EVENT_NAME.NOTE_DELETED, {
-				permanently: 'true',
-			});
-		},
-	);
-
-	useWorkspaceCommandCallback(GLOBAL_COMMANDS.RESTORE_NOTE_FROM_BIN, async ({ id }) => {
-		await notes.updateMeta([id], { isDeleted: false });
-		const updatedNote = await notes.getById(id);
-		if (updatedNote) noteUpdated(updatedNote);
-
-		updateNotes();
-
-		telemetry.track(TELEMETRY_EVENT_NAME.NOTE_RESTORED_FROM_BIN);
-	});
-};
-
 export const useNoteContextMenu = ({ updateNotes }: ContextMenuOptions) => {
 	const telemetry = useTelemetryTracker();
 
 	const notes = useNotesRegistry();
 	const tagsRegistry = useTagsRegistry();
-
-	const notesExport = useNotesExport();
-	const currentWorkspace = useWorkspaceData();
-	const workspaceData = useAppSelector(selectWorkspace(currentWorkspace));
 
 	const runCommand = useCommand();
 
@@ -192,15 +119,7 @@ export const useNoteContextMenu = ({ updateNotes }: ContextMenuOptions) => {
 				},
 
 				[NoteActions.EXPORT]: async (id: string) => {
-					const note = await notes.getById(id);
-					await notesExport.exportNote(
-						id,
-						buildFileName(
-							workspaceData?.name,
-							note?.content.title.trim().slice(0, 50).trim() ||
-								`note_${id}`,
-						),
-					);
+					runCommand(GLOBAL_COMMANDS.EXPORT_NOTE, { id });
 				},
 			};
 
@@ -208,15 +127,7 @@ export const useNoteContextMenu = ({ updateNotes }: ContextMenuOptions) => {
 				actionsMap[action](id);
 			}
 		},
-		[
-			telemetry,
-			runCommand,
-			notes,
-			tagsRegistry,
-			updateNotes,
-			notesExport,
-			workspaceData?.name,
-		],
+		[telemetry, runCommand, notes, tagsRegistry, updateNotes],
 	);
 
 	const showMenu = useShowNoteContextMenu(noteContextMenuCallback);
