a0a53dcf1d5c5478f0f6e1b351d55a19bf25e1e5
Irina Katsyuta <katsyuta@proton.me>
2026-01-13 14:11:54 +0100

    chore: rename

--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -98,7 +98,7 @@ export const App: FC = () => {
 				profilesManager={profilesList}
 				currentProfile={currentProfile}
 				onChooseProfile={setCurrentProfile}
-				screenMode={profileScreen}
+				screenName={profileScreen}
 			/>
 		);
 	}
