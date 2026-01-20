70d45917de4f87d0d458e1e0af976bd7dcd4ca72
Irina Katsyuta <katsyuta@proton.me>
2026-01-20 13:55:18 +0100

    refactor: move handler to decrease time to open note

--- src/features/MainScreen/NotesListPanel/NotesList.tsx
+++ src/features/MainScreen/NotesListPanel/NotesList.tsx
@@ -1,10 +1,8 @@
 import React, { FC, useLayoutEffect, useRef } from 'react';
 import { Box, Text, VStack } from '@chakra-ui/react';
 import { NotePreview } from '@components/NotePreview/NotePreview';
-import { TELEMETRY_EVENT_NAME } from '@core/features/telemetry';
 import { getContextMenuCoords } from '@electron/requests/contextMenu/renderer';
 import { useNoteContextMenu } from '@features/NotesContainer/NoteContextMenu/useNoteContextMenu';
-import { useTelemetryTracker } from '@features/telemetry';
 import { useNoteActions } from '@hooks/notes/useNoteActions';
 import { useUpdateNotes } from '@hooks/notes/useUpdateNotes';
 import { useIsActiveWorkspace } from '@hooks/useIsActiveWorkspace';
@@ -20,8 +18,6 @@ import { isElementInViewport } from '@utils/dom/isElementInViewport';
 export type NotesListProps = {};
 
 export const NotesList: FC<NotesListProps> = () => {
-	const telemetry = useTelemetryTracker();
-
 	const noteActions = useNoteActions();
 
 	const activeNoteId = useWorkspaceSelector(selectActiveNoteId);
@@ -68,14 +64,14 @@ export const NotesList: FC<NotesListProps> = () => {
 			const item = virtualizer.getVirtualItems().find((i) => i.index === noteIndex);
 
 			if (item) {
-				const domNode =
+				const activeNode =
 					activeNoteRef.current ??
 					parentRef.current?.querySelector<HTMLElement>(
 						`[data-index="${noteIndex}"]`,
 					);
 
-				if (domNode) {
-					domNode.scrollIntoView({ block: 'center', behavior: 'auto' });
+				if (activeNode) {
+					activeNode.scrollIntoView({ block: 'center', behavior: 'auto' });
 				} else {
 					virtualizer.scrollToOffset(item.start);
 				}
