86b09471a743044abf66a874706e7343fcffccb7
Robert Vitonsky <robert@vitonsky.net>
2026-01-21 14:20:16 +0100

    fix: scale window content

--- src/windows/main/preload.ts
+++ src/windows/main/preload.ts
@@ -1,4 +1,11 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { contextBridge, ipcRenderer, webFrame } from 'electron';
+
+webFrame.setZoomFactor(1);
+
+const dpr = window.devicePixelRatio;
+
+console.log('Device pixel ratio', dpr);
+webFrame.setZoomFactor(dpr);
 
 contextBridge.exposeInMainWorld('electron', {
 	ipcRenderer: {
