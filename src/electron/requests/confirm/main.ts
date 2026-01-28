928b2f38b346cefa9b6d38d3300a2b240aa89430
Irina Katsyuta <katsyuta@proton.me>
2026-01-28 20:33:11 +0100

    chore: rename

--- src/electron/requests/confirm/main.ts
+++ src/electron/requests/confirm/main.ts
@@ -2,7 +2,7 @@ import { BrowserWindow, dialog, ipcMain } from 'electron';
 
 // Patch confirm: original window.confirm causes focus loss; showMessageBoxSync keeps it modal
 export const enableConfirm = () => {
-	ipcMain.on('show-confirm', (event, message?: string) => {
+	ipcMain.on('showConfirm', (event, message?: string) => {
 		const targetWindow = BrowserWindow.fromWebContents(event.sender);
 		if (!targetWindow) return;
 
