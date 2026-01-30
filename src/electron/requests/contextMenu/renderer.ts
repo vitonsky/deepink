d683872ac3f22e99969a196829daeafdc67ceddc
Robert Vitonsky <robert@vitonsky.net>
2026-01-30 11:42:55 +0100

    fix: context menu appears on middle of screen (#213)
    * fix: consider zoom factor
    
    * chore: add description

--- src/electron/requests/contextMenu/renderer.ts
+++ src/electron/requests/contextMenu/renderer.ts
@@ -1,3 +1,5 @@
+import { getZoomFactor } from '@utils/os/zoom';
+
 import { ipcRendererFetcher } from '../../utils/ipc/ipcRendererFetcher';
 
 import { ContextMenu, contextMenuChannel } from '.';
@@ -10,7 +12,12 @@ export type ContextMenuRequestProps = {
 
 export const { open: openContextMenu } = contextMenuChannel.client(ipcRendererFetcher);
 
-export const getContextMenuCoords = (event: MouseEvent) => ({
-	x: event.screenX,
-	y: event.screenY,
-});
+export const getContextMenuCoords = (event: MouseEvent) => {
+	// Consider zoom factor to draw context menu on proper coordinates for scaled windows
+	// With no zoom factor, context menu will be rendered under pointer, and click random option
+	const zoomFactor = getZoomFactor();
+	return {
+		x: Math.ceil(event.pageX * zoomFactor),
+		y: Math.ceil(event.pageY * zoomFactor),
+	};
+};
