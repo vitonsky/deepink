b3cacbaa9846ef861b72bede75ec077159edd65b
Robert Vitonsky <robert@vitonsky.net>
2026-02-05 22:24:36 +0100

    fix: show tooltip on slider while dragging

--- src/components/Slider/SimpleSlider.tsx
+++ src/components/Slider/SimpleSlider.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import {
 	Slider,
 	SliderFilledTrack,
@@ -14,15 +14,37 @@ export type SimpleSliderProps = Omit<SliderProps, 'defaultValue'> &
 		transformValue?: (value: number) => string;
 	};
 
+type SliderState = { hover: boolean; dragging: boolean };
+
 /**
  * Simple to use slider control
  * Under the hood is a https://v2.chakra-ui.com/docs/components/slider with sensible defaults
  */
 
 export const SimpleSlider = ({ transformValue, ...props }: SimpleSliderProps) => {
+	const [state, updateState] = useReducer<SliderState, [Partial<SliderState>]>(
+		(state, changes) => {
+			return { ...state, ...changes };
+		},
+		{
+			hover: false,
+			dragging: false,
+		},
+	);
+
 	const { value, min, max } = props;
 	return (
-		<Slider {...props}>
+		<Slider
+			{...props}
+			onMouseEnter={() => updateState({ hover: true })}
+			onMouseLeave={() => updateState({ hover: false })}
+			onChangeStart={() => {
+				updateState({ dragging: true });
+			}}
+			onChangeEnd={() => {
+				updateState({ dragging: false });
+			}}
+		>
 			<SliderMark value={min} mt="1" fontSize="sm" color="typography.secondary">
 				{transformValue ? transformValue(min) : min}
 			</SliderMark>
@@ -42,6 +64,7 @@ export const SimpleSlider = ({ transformValue, ...props }: SimpleSliderProps) =>
 				hasArrow
 				placement="top"
 				label={transformValue ? transformValue(value) : value}
+				isOpen={state.dragging || state.hover}
 			>
 				<SliderThumb />
 			</Tooltip>
