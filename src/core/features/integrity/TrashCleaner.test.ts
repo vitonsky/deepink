8e287d21c3bf4dfff26ba584c526222496d8df95
Robert Vitonsky <robert@vitonsky.net>
2026-02-06 16:21:07 +0100

    feat: consider a note mod time in bin retention policy

--- src/core/features/integrity/TrashCleaner.test.ts
+++ src/core/features/integrity/TrashCleaner.test.ts
@@ -65,3 +65,48 @@ test('Purge a notes in bin deleted later than retention time', async () => {
 	await expect(bin.purgeExpired(), 'Expired notes has been deleted').resolves.toBe(30);
 	await expect(getNoteIds(notesController)).resolves.toEqual([]);
 });
+
+test('Modification time is considered when required', async () => {
+	const db = await openDatabase(dbFile);
+	const notesController = new NotesController(db, FAKE_WORKSPACE_ID);
+	const bin = new TrashCleaner(
+		{ notes: notesController },
+		{ retentionTime: ms('10d'), considerModificationTime: true },
+	);
+
+	// Create notes
+	vi.setSystemTime('2020-01-01T15:00:00.000Z');
+	const noteIds = await createNotes(notesController, 100);
+	expect(noteIds).toHaveLength(100);
+
+	// Move to bin
+	await notesController.updateMeta(noteIds, { isDeleted: true });
+
+	// Purge
+	await expect(bin.purgeExpired(), 'Nothing to purge').resolves.toBe(0);
+
+	vi.setSystemTime('2020-01-11T14:59:59.999Z');
+	await expect(bin.purgeExpired(), 'No expired notes yet').resolves.toBe(0);
+
+	// Update deletion time for part of notes
+	vi.setSystemTime('2020-01-11T15:00:00.000Z');
+
+	const noteId = noteIds[0];
+	await notesController.update(noteId, { title: '', text: '' });
+
+	await expect(bin.purgeExpired(), 'Expired notes has been deleted').resolves.toBe(99);
+	await expect(getNoteIds(notesController)).resolves.toEqual([noteId]);
+
+	vi.setSystemTime('2021-01-11T15:00:00.000Z');
+	await notesController.update(noteId, { title: '', text: '' });
+	await expect(bin.purgeExpired(), 'Nothing to purge').resolves.toBe(0);
+	await expect(getNoteIds(notesController)).resolves.toEqual([noteId]);
+
+	vi.setSystemTime('2021-01-20T15:00:00.000Z');
+	await expect(bin.purgeExpired(), 'Nothing to purge').resolves.toBe(0);
+	await expect(getNoteIds(notesController)).resolves.toEqual([noteId]);
+
+	vi.setSystemTime('2021-01-21T15:00:00.000Z');
+	await expect(bin.purgeExpired(), 'Retention policy is expired').resolves.toBe(1);
+	await expect(getNoteIds(notesController)).resolves.toEqual([]);
+});
--- src/core/features/integrity/TrashCleaner.ts
+++ src/core/features/integrity/TrashCleaner.ts
@@ -10,21 +10,37 @@ export class TrashCleaner {
 		},
 		private readonly config: {
 			retentionTime: number;
+			considerModificationTime?: boolean;
 		},
 	) {}
 
 	public async purgeExpired() {
 		const { notes } = this.controllers;
-		const { retentionTime } = this.config;
+		const { retentionTime, considerModificationTime } = this.config;
 
 		// TODO: use `query` method to fetch only ids
+		const validityDate = Date.now() - retentionTime;
 		const noteIds = await notes
 			.get({
 				deletedAt: {
-					to: new Date(Date.now() - retentionTime),
+					to: new Date(validityDate),
 				},
 			})
-			.then((notes) => notes.map((note) => note.id));
+			.then((notes) =>
+				notes
+					.values()
+					.filter((note) => {
+						if (
+							considerModificationTime &&
+							note.updatedTimestamp !== undefined &&
+							note.updatedTimestamp > validityDate
+						)
+							return false;
+						return true;
+					})
+					.map((note) => note.id)
+					.toArray(),
+			);
 
 		await notes.delete(noteIds);
 
