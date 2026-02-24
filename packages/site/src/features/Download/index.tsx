ef637ff525073bf57c30e2c3a18638ab7f475dd6
Vitonsky <rob@vitonsky.net>
2026-02-24 22:19:32 +0100

    feat: localize download page

--- packages/site/src/features/Download/index.tsx
+++ packages/site/src/features/Download/index.tsx
@@ -1,9 +1,10 @@
 import React, { Fragment, type ReactNode, useEffect, useMemo, useState } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 import { BiCloudDownload } from 'react-icons/bi';
 import { FaApple, FaLinux, FaWindows } from 'react-icons/fa6';
 import { Box, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
 
-import Layout, { type LayoutProps } from '../../components/Layout/Layout';
+import { WithLayout } from '../../components/Layout';
 import { Link } from '../../components/Link';
 import { Text } from '../../components/Text';
 import { TheRock } from '../../components/TheRock';
@@ -55,10 +56,9 @@ type DownloadGroup = {
 	}[];
 };
 
-export default function Page({
-	i18n,
+export default WithLayout(function Page({
 	versions,
-}: Pick<LayoutProps, 'i18n'> & {
+}: {
 	versions: {
 		url: string;
 		name: string;
@@ -70,6 +70,11 @@ export default function Page({
 		}[];
 	}[];
 }) {
+	const {
+		t,
+		i18n: { language },
+	} = useTranslation('downloads');
+
 	const { downloads, linkMap } = useMemo(() => {
 		const msi = versions[0].assets.find((version) =>
 			version.name.endsWith('.msi'),
@@ -136,75 +141,77 @@ export default function Page({
 		const date = versions[0].publishedAt;
 		if (!date) return null;
 
-		return new Intl.DateTimeFormat('en', {
+		return new Intl.DateTimeFormat(language, {
 			year: 'numeric',
 			month: 'long',
 			day: 'numeric',
 		}).format(new Date(date));
-	}, [versions]);
+	}, [language, versions]);
 
 	return (
-		<Layout i18n={i18n}>
-			<VStack paddingBlock="8rem" justifyContent="center" gap="3rem">
-				<VStack gap="3rem">
-					<TheRock maxW="100%" width="350px" />
+		<VStack paddingBlock="8rem" justifyContent="center" gap="3rem">
+			<VStack gap="3rem">
+				<TheRock maxW="100%" width="350px" />
 
-					<VStack gap="1rem">
-						<Link variant="button-primary" href={downloadLink}>
-							Download for <PlatformName />
-						</Link>
-						{lastReleaseDate && (
-							<Text variant="description" fontFamily="monospace">
-								Last updated {lastReleaseDate}
-							</Text>
-						)}
-					</VStack>
-				</VStack>
-
-				<VStack
-					maxWidth="500px"
-					width="100%"
-					padding="2rem"
-					border="3px solid"
-					borderColor="border.contrast"
-					borderRadius="6px"
-					gap="3rem"
-				>
-					<Heading>Download links</Heading>
-					<SimpleGrid
-						columns={2}
-						width="100%"
-						fontSize="20px"
-						css={{
-							rowGap: '1.5rem',
-							'& > *:not(:nth-last-child(-n + 2))': {
-								paddingBottom: '1.5rem',
-								borderBottom: '1px solid',
-								borderColor: 'border.thin',
-							},
-						}}
-					>
-						{downloads.map((section) => (
-							<Fragment key={section.title}>
-								<Text as={HStack} textAlign="start" gap=".3em">
-									{section.icon}
-									<span>{section.title}</span>
-								</Text>
-								<VStack align="start">
-									{section.links.map((link) => (
-										<Link key={link.url} href={link.url}>
-											<HStack gap=".3em">
-												<Box as={BiCloudDownload} />
-												<span>{link.title}</span>
-											</HStack>
-										</Link>
-									))}
-								</VStack>
-							</Fragment>
-						))}
-					</SimpleGrid>
+				<VStack gap="1rem">
+					<Link variant="button-primary" href={downloadLink}>
+						<Trans
+							t={t}
+							i18nKey="main.download"
+							components={[<PlatformName key={0} />]}
+						/>
+					</Link>
+					{lastReleaseDate && (
+						<Text variant="description" fontFamily="monospace">
+							{t('main.releaseDate', { date: lastReleaseDate })}
+						</Text>
+					)}
 				</VStack>
 			</VStack>
-		</Layout>
+
+			<VStack
+				maxWidth="500px"
+				width="100%"
+				padding="2rem"
+				border="3px solid"
+				borderColor="border.contrast"
+				borderRadius="6px"
+				gap="3rem"
+			>
+				<Heading>{t('links.title')}</Heading>
+				<SimpleGrid
+					columns={2}
+					width="100%"
+					fontSize="20px"
+					css={{
+						rowGap: '1.5rem',
+						'& > *:not(:nth-last-child(-n + 2))': {
+							paddingBottom: '1.5rem',
+							borderBottom: '1px solid',
+							borderColor: 'border.thin',
+						},
+					}}
+				>
+					{downloads.map((section) => (
+						<Fragment key={section.title}>
+							<Text as={HStack} textAlign="start" gap=".3em">
+								{section.icon}
+								<span>{section.title}</span>
+							</Text>
+							<VStack align="start">
+								{section.links.map((link) => (
+									<Link key={link.url} href={link.url}>
+										<HStack gap=".3em">
+											<Box as={BiCloudDownload} />
+											<span>{link.title}</span>
+										</HStack>
+									</Link>
+								))}
+							</VStack>
+						</Fragment>
+					))}
+				</SimpleGrid>
+			</VStack>
+		</VStack>
 	);
-}
+});
