8da7a6474f6ce273e83be55c560a83264986b2a8
Irina Katsyuta <katsyuta@proton.me>
2026-02-26 15:06:52 +0100

    refactor: move to component

---
+++ src/features/App/VaultScreenManager.tsx
@@ -0,0 +1,132 @@
+import React, { useMemo, useState } from 'react';
+import { FaUser } from 'react-icons/fa6';
+import { Button, Divider, HStack, Text } from '@chakra-ui/react';
+import { NestedList } from '@components/NestedList';
+import { TELEMETRY_EVENT_NAME } from '@core/features/telemetry';
+import { telemetry } from '@electron/requests/telemetry/renderer';
+import { getRandomItem } from '@utils/collections/getRandomItem';
+
+import { ProfileCreator } from './Profile/ProfileCreator';
+import { ProfileLoginForm } from './Profile/ProfileLoginForm';
+import { ProfilesForm } from './Profile/ProfilesForm';
+import { OnPickProfile } from './useProfileLoader';
+import { ProfilesListApi } from './useProfilesList';
+
+export type VaultScreenManagerProps = {
+	profiles: ProfilesListApi;
+	currentProfile: string | null;
+	onChooseProfile: (id: string | null) => void;
+	onOpenProfile: OnPickProfile;
+	setIsManualOpen: () => void;
+};
+
+export const defaultVaultNames = [
+	'Creative drafts',
+	'Second brain',
+	'Digital garden',
+	'Creative space',
+	'Mind space',
+	'Idea lab',
+];
+
+export const VaultScreenManager = ({
+	currentProfile,
+	profiles,
+	onOpenProfile,
+	onChooseProfile,
+	setIsManualOpen,
+}: VaultScreenManagerProps) => {
+	const currentProfileObject = useMemo(
+		() => profiles.profiles?.find((profile) => profile.id === currentProfile) ?? null,
+		[currentProfile, profiles.profiles],
+	);
+
+	const [screen, setScreen] = useState<'create' | 'choose'>('choose');
+	const hasNoProfiles = profiles.profiles.length === 0;
+
+	if (currentProfileObject) {
+		return (
+			<ProfileLoginForm
+				profile={currentProfileObject}
+				onLogin={async (...arg) => {
+					setIsManualOpen();
+					return await onOpenProfile(...arg);
+				}}
+				onPickAnotherProfile={() => onChooseProfile(null)}
+			/>
+		);
+	}
+
+	if (screen === 'create' || hasNoProfiles) {
+		return (
+			<ProfileCreator
+				onCreateProfile={(profile) =>
+					profiles.createProfile(profile).then((newProfile) => {
+						onOpenProfile(newProfile, profile.password ?? undefined).then(
+							console.warn,
+						);
+						setScreen('choose');
+					})
+				}
+				onCancel={hasNoProfiles ? undefined : () => setScreen('choose')}
+				defaultProfileName={
+					hasNoProfiles ? getRandomItem(defaultVaultNames) : undefined
+				}
+			/>
+		);
+	}
+
+	return (
+		<ProfilesForm
+			title="Choose the profile"
+			controls={
+				<Button
+					variant="accent"
+					size="lg"
+					w="100%"
+					onClick={() => setScreen('create')}
+				>
+					Create new profile
+				</Button>
+			}
+		>
+			<NestedList
+				divider={<Divider margin="0px !important" />}
+				sx={{
+					w: '100%',
+					borderRadius: '4px',
+					maxHeight: '230px',
+					overflow: 'auto',
+					border: '1px solid',
+				}}
+				items={profiles.profiles.map((profile) => ({
+					id: profile.id,
+					content: (
+						<HStack
+							as="button"
+							key={profile.id}
+							sx={{
+								padding: '.8rem 1rem',
+								w: '100%',
+								cursor: 'pointer',
+								gap: '.8rem',
+							}}
+							onClick={() => {
+								onChooseProfile(profile.id);
+
+								if (profile.encryption === null) {
+									onOpenProfile(profile);
+								}
+
+								telemetry.track(TELEMETRY_EVENT_NAME.PROFILE_SELECTED);
+							}}
+						>
+							<FaUser />
+							<Text>{profile.name}</Text>
+						</HStack>
+					),
+				}))}
+			/>
+		</ProfilesForm>
+	);
+};
--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,33 +1,17 @@
-import React, { FC, useEffect, useMemo, useState } from 'react';
-import { FaUser } from 'react-icons/fa6';
-import { Box, Button, Divider, HStack, Text } from '@chakra-ui/react';
-import { NestedList } from '@components/NestedList';
-import { TELEMETRY_EVENT_NAME } from '@core/features/telemetry';
+import React, { FC, useEffect, useState } from 'react';
+import { Box } from '@chakra-ui/react';
 import { ConfigStorage } from '@core/storage/ConfigStorage';
-import { telemetry } from '@electron/requests/telemetry/renderer';
 import { useFilesStorage } from '@features/files';
 import { SplashScreen } from '@features/SplashScreen';
-import { getRandomItem } from '@utils/collections/getRandomItem';
 
 import { AppServices } from './AppServices';
-import { ProfileCreator } from './Profile/ProfileCreator';
-import { ProfileLoginForm } from './Profile/ProfileLoginForm';
-import { ProfilesForm } from './Profile/ProfilesForm';
 import { Profiles } from './Profiles';
 import { useProfileContainers } from './Profiles/hooks/useProfileContainers';
 import { useProfileLoader } from './useProfileLoader';
 import { useProfileSelector } from './useProfileSelector';
 import { useProfilesList } from './useProfilesList';
 import { useRecentProfile } from './useRecentProfile';
-
-const defaultVaultNames = [
-	'Creative drafts',
-	'Second brain',
-	'Digital garden',
-	'Creative space',
-	'Mind space',
-	'Idea lab',
-];
+import { VaultScreenManager } from './VaultScreenManager';
 
 /**
  * Immediately returns true, but waits delay time before switching to false
@@ -56,12 +40,6 @@ export const App: FC = () => {
 	const profileContainers = useProfileContainers();
 
 	const [currentProfileId, setCurrentProfileId] = useProfileSelector(config);
-	const currentProfileObject = useMemo(
-		() =>
-			profilesList.profiles?.find((profile) => profile.id === currentProfileId) ??
-			null,
-		[currentProfileId, profilesList.profiles],
-	);
 
 	// Open recent profile
 	const recentProfile = useRecentProfile(config);
@@ -72,15 +50,11 @@ export const App: FC = () => {
 		profiles: profileContainers,
 	});
 
-	const [vaultManagerScreen, setVaultManagerScreen] = useState<'create' | 'choose'>(
-		'choose',
-	);
-
-	const [openMode, setOpenMode] = useState<'auto' | 'manual'>('auto');
+	const [isManualOpen, setIsManualOpen] = useState(false);
 	const isLoading =
 		!profilesList.isProfilesLoaded ||
 		!recentProfile.isLoaded ||
-		(isProfileOpening && openMode === 'auto');
+		(isProfileOpening && !isManualOpen);
 
 	const isShowSplash = useMinimumVisible(isLoading, 400);
 	if (isShowSplash) {
@@ -103,104 +77,16 @@ export const App: FC = () => {
 		);
 	}
 
-	// Managing vault screens
-	const hasNoProfiles = profilesList.profiles.length === 0;
-	const screen = currentProfileObject
-		? 'login'
-		: vaultManagerScreen === 'create' || hasNoProfiles
-			? 'create'
-			: 'choose';
 	return (
 		<Box display="flex" minH="100vh" justifyContent="center" alignItems="center">
 			<Box maxW="500px" minW="350px" padding="1rem">
-				{screen === 'login' && currentProfileObject && (
-					<ProfileLoginForm
-						profile={currentProfileObject}
-						onLogin={async (...arg) => {
-							setOpenMode('manual');
-							return await onOpenProfile(...arg);
-						}}
-						onPickAnotherProfile={() => setCurrentProfileId(null)}
-					/>
-				)}
-
-				{screen === 'create' && (
-					<ProfileCreator
-						onCreateProfile={(profile) =>
-							profilesList.createProfile(profile).then((newProfile) => {
-								onOpenProfile(
-									newProfile,
-									profile.password ?? undefined,
-								).then(console.warn);
-								setVaultManagerScreen('choose');
-							})
-						}
-						onCancel={
-							hasNoProfiles
-								? undefined
-								: () => setVaultManagerScreen('choose')
-						}
-						defaultProfileName={
-							hasNoProfiles ? getRandomItem(defaultVaultNames) : undefined
-						}
-					/>
-				)}
-
-				{screen === 'choose' && (
-					<ProfilesForm
-						title="Choose the profile"
-						controls={
-							<Button
-								variant="accent"
-								size="lg"
-								w="100%"
-								onClick={() => setVaultManagerScreen('create')}
-							>
-								Create new profile
-							</Button>
-						}
-					>
-						<NestedList
-							divider={<Divider margin="0px !important" />}
-							sx={{
-								w: '100%',
-								borderRadius: '4px',
-								maxHeight: '230px',
-								overflow: 'auto',
-								border: '1px solid',
-							}}
-							items={profilesList.profiles.map((profile) => ({
-								id: profile.id,
-								content: (
-									<HStack
-										as="button"
-										key={profile.id}
-										sx={{
-											padding: '.8rem 1rem',
-											w: '100%',
-											cursor: 'pointer',
-											gap: '.8rem',
-										}}
-										onClick={() => {
-											setCurrentProfileId(profile.id);
-
-											if (profile.encryption === null) {
-												onOpenProfile(profile);
-											}
-
-											telemetry.track(
-												TELEMETRY_EVENT_NAME.PROFILE_SELECTED,
-											);
-										}}
-									>
-										<FaUser />
-										<Text>{profile.name}</Text>
-									</HStack>
-								),
-							}))}
-						/>
-					</ProfilesForm>
-				)}
+				<VaultScreenManager
+					currentProfile={currentProfileId}
+					profiles={profilesList}
+					onOpenProfile={onOpenProfile}
+					onChooseProfile={setCurrentProfileId}
+					setIsManualOpen={() => setIsManualOpen(true)}
+				/>
 			</Box>
 		</Box>
 	);
