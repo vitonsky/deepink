2606eea1cba94c7c1aecedb0fce7fd521082da78
Vitonsky <rob@vitonsky.net>
2026-02-28 00:07:31 +0100

    chore: add favicon to docs

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -61,6 +61,7 @@ export default defineConfig({
 		starlight({
 			plugins: [starlightThemeFlexoki()],
 			title: 'Deepink',
+			favicon: '/favicon.svg',
 			logo: {
 				replacesTitle: true,
 				src: './src/components/Layout/logo.svg',
