1078f216b04851ffd0db8420b5842fbf0f96fd23
Vitonsky <rob@vitonsky.net>
2026-02-27 20:44:47 +0100

    chore: watch i18n files in dev mode

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-unresolved */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
+import path from 'node:path';
+
 import { defineConfig } from 'astro/config';
 import remarkExternalLinks from 'remark-external-links';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 import react from '@astrojs/react';
 import starlight from '@astrojs/starlight';
 
@@ -15,36 +18,68 @@ export default defineConfig({
             // Fix build error. See details at https://github.com/withastro/astro/issues/14117#issuecomment-3117797751
             noExternal: ['zod'],
         },
-        plugins: [svgr({
-            include: '**/*.svg?react',
-            svgrOptions: {
-                plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
-                svgoConfig: {
-                    plugins: ['preset-default', 'removeTitle', 'removeDesc', 'removeDoctype', 'cleanupIds'],
+        plugins: [
+            svgr({
+                include: '**/*.svg?react',
+                svgrOptions: {
+                    plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+                    svgoConfig: {
+                        plugins: [
+                            'preset-default',
+                            'removeTitle',
+                            'removeDesc',
+                            'removeDoctype',
+                            'cleanupIds',
+                        ],
+                    },
+                },
+            }),
+            {
+                name: 'watch-external-dir',
+                configureServer(server) {
+                    const watchedDir = path.resolve(`src/i18n/locales`);
+
+                    // Tell Vite to watch this directory
+                    server.watcher.add(watchedDir);
+
+                    server.watcher.on('change', (file) => {
+                        if (file.startsWith(watchedDir)) {
+                            server.ws.send({ type: 'full-reload' });
+                        }
+                    });
                 },
             },
-        })]
+        ],
     },
     markdown: {
         remarkPlugins: [
-            [remarkExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
-        ]
-    },
-    integrations: [starlight({
-        title: 'My Docs',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
+            [remarkExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
         ],
-    }), react()],
+    },
+    integrations: [
+        starlight({
+            title: 'My Docs',
+            social: [
+                {
+                    icon: 'github',
+                    label: 'GitHub',
+                    href: 'https://github.com/withastro/starlight',
+                },
+            ],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+        react(),
+    ],
 });
