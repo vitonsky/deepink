9f998bd5202845e6dc7801aeb2360a8e6ad3da0e
Robert Vitonsky <robert@vitonsky.net>
2026-03-02 14:42:43 +0100

    chore: comments

--- packages/site/src/components/Layout/Logo.tsx
+++ packages/site/src/components/Layout/Logo.tsx
@@ -3,7 +3,6 @@ import { Box } from '@chakra-ui/react';
 
 import LogoIcon from './logo.svg?react';
 
-// TODO: use just an SVG element
 export const Logo = () => {
 	return <Box as={LogoIcon} display="inline-block" width="8em" height="auto" />;
 };
