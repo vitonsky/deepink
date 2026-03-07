a7186f331afe745e58095497312c4ddf9d603aa1
Robert Vitonsky <robert@vitonsky.net>
2026-03-07 14:14:36 +0300

    fix: app cannot fetch updates because of GitHub rate limits (#243)
    * feat: use site to check new releases
    
    * test: cover real json usage
    
    * feat: use site download page for updates
    
    * chore: do not mutate object
    
    * test: network errors
    
    * test: invalid versions are ignored
    
    * fix: find the latest valid version

---
+++ src/electron/updates/AppUpdatesChecker.ts
@@ -0,0 +1,52 @@
+import semver from 'semver';
+import { z } from 'zod';
+
+const VersionObjectScheme = z.object({
+	url: z.string(),
+	name: z.string(),
+	prerelease: z.boolean(),
+});
+
+export type VersionObject = z.output<typeof VersionObjectScheme>;
+
+export type AppVersionInfo = {
+	version: string;
+	url: string;
+};
+
+export class AppUpdatesChecker {
+	constructor(
+		private readonly config: {
+			host: string;
+		},
+	) {}
+
+	public async getUpdate(context: { version: string }): Promise<AppVersionInfo | null> {
+		const { host } = this.config;
+
+		try {
+			const response = await fetch(new URL('/versions.json', host).toString());
+			if (!response.ok) throw new Error(response.statusText);
+
+			const json = await response.json();
+			const releases = VersionObjectScheme.array().parse(json);
+
+			const latestRelease = releases.find((version) => semver.valid(version.name));
+			if (!latestRelease) return null;
+
+			const latestVersion = latestRelease.name.replace(/^v/, '');
+			const isNewVersion = semver.gt(latestVersion, context.version);
+
+			return isNewVersion
+				? {
+						version: latestVersion,
+						url: latestRelease.url,
+					}
+				: null;
+		} catch (error) {
+			console.error(error);
+
+			return null;
+		}
+	}
+}
---
+++ src/electron/updates/__tests__/AppUpdatesChecker.test.ts
@@ -0,0 +1,94 @@
+import { AppUpdatesChecker } from '../AppUpdatesChecker';
+import versions from './versions.json';
+
+const mockFetch = vi.fn();
+globalThis.fetch = mockFetch;
+
+const mockServerResponse = (payload: any) => {
+	mockFetch.mockImplementation(
+		() => new Response(JSON.stringify(payload), { status: 200 }),
+	);
+};
+
+beforeEach(() => {
+	vi.clearAllMocks();
+	mockServerResponse(versions);
+});
+
+const updatesChecker = new AppUpdatesChecker({ host: 'https://site.com' });
+
+test('Greater version must be considered as new version', async () => {
+	const latestVersion = {
+		url: 'https://github.com/vitonsky/deepink/releases/tag/v0.0.2-preview.8',
+		version: '0.0.2-preview.8',
+	};
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.1',
+		}),
+	).resolves.toEqual(latestVersion);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.1-preview.1',
+		}),
+	).resolves.toEqual(latestVersion);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.1-preview.999',
+		}),
+	).resolves.toEqual(latestVersion);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.2-preview.1',
+		}),
+	).resolves.toEqual(latestVersion);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.2-preview.7',
+		}),
+	).resolves.toEqual(latestVersion);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.2-preview.8',
+		}),
+	).resolves.toEqual(null);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.2-preview.9',
+		}),
+	).resolves.toEqual(null);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '1.0.0',
+		}),
+	).resolves.toEqual(null);
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '1.1.1',
+		}),
+	).resolves.toEqual(null);
+});
+
+test('Returns null for a network errors', async () => {
+	const spyError = vi.spyOn(console, 'error');
+	mockFetch.mockImplementation(() => {
+		throw new Error('Network error emulation');
+	});
+
+	await expect(
+		updatesChecker.getUpdate({
+			version: '0.0.1',
+		}),
+	).resolves.toEqual(null);
+
+	expect(spyError).toBeCalledWith(new Error('Network error emulation'));
+});
