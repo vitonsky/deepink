4a3355733ba8a68e47bd6974b78cacc6c81f53f5
Robert Vitonsky <robert@vitonsky.net>
2026-03-24 09:11:08 +0100

    refactor: extend OverlayFS

--- src/core/features/files/ComlinkFS.ts
+++ src/core/features/files/ComlinkFS.ts
@@ -1,40 +1,17 @@
 import { transfer } from 'comlink';
 
+import { OverlayFS } from './OverlayFS';
 import { IFilesStorage } from '.';
 
-export class ComlinkHostFS implements IFilesStorage {
-	constructor(protected readonly storage: IFilesStorage) {}
-
-	async write(path: string, buffer: ArrayBuffer) {
-		await this.storage.write(path, buffer);
-	}
-
+export class ComlinkHostFS extends OverlayFS implements IFilesStorage {
 	async get(path: string) {
 		const result = await this.storage.get(path);
 		return result ? transfer(result, [result]) : null;
 	}
-	async delete(paths: string[]) {
-		await this.storage.delete(paths);
-	}
-	async list() {
-		return this.storage.list();
-	}
 }
 
-export class ComlinkWorkerFS implements IFilesStorage {
-	constructor(protected readonly storage: IFilesStorage) {}
-
+export class ComlinkWorkerFS extends OverlayFS implements IFilesStorage {
 	async write(path: string, buffer: ArrayBuffer) {
 		await this.storage.write(path, transfer(buffer, [buffer]));
-	}
-
-	async get(path: string) {
-		return this.storage.get(path);
-	}
-	async delete(paths: string[]) {
-		return this.storage.delete(paths);
-	}
-	async list() {
-		return this.storage.list();
 	}
 }
