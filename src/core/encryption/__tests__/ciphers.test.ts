8072774044aa47f3d08a02379ee6f89388997b42
Robert Vitonsky <robert@vitonsky.net>
2026-03-30 23:06:46 +0200

    test: cover ciphers combinations

--- src/core/encryption/__tests__/ciphers.test.ts
+++ src/core/encryption/__tests__/ciphers.test.ts
@@ -3,6 +3,9 @@ import { webcrypto } from 'node:crypto';
 import { AESGCMCipher } from '../ciphers/AES';
 import { SeprentCipher } from '../ciphers/Serpent';
 import { WasmTwofishCTRCipher } from '../ciphers/Twofish';
+import { BufferIntegrityProcessor } from '../processors/BufferIntegrityProcessor';
+import { PipelineProcessor } from '../processors/PipelineProcessor';
+import { getDerivedKeysManager, getMasterKey } from '../utils/keys';
 import { getRandomBytes } from '../utils/random';
 import { IEncryptionProcessor, RandomBytesGenerator } from '..';
 
@@ -41,6 +44,41 @@ const ciphers: {
 			return new SeprentCipher(key, randomBytes);
 		},
 	},
+	{
+		name: 'AES-Twofish-Serpent',
+		async create(key, randomBytes) {
+			const salt = new Uint8Array(32);
+			const derivedKeys = await getMasterKey(key.buffer, salt.buffer).then(
+				(masterKey) => getDerivedKeysManager(masterKey, salt),
+			);
+
+			return new PipelineProcessor([
+				new BufferIntegrityProcessor(),
+				...(await Promise.all([
+					derivedKeys
+						.getDerivedKey('AES', {
+							name: 'AES-GCM',
+							length: 256,
+						})
+						.then((key) => new AESGCMCipher(key, randomBytes)),
+					derivedKeys
+						.getDerivedBytes('Twofish', 32 * 8)
+						.then(
+							(key) =>
+								new WasmTwofishCTRCipher(
+									new Uint8Array(key),
+									randomBytes,
+								),
+						),
+					derivedKeys
+						.getDerivedBytes('Serpent', 32 * 8)
+						.then(
+							(key) => new SeprentCipher(new Uint8Array(key), randomBytes),
+						),
+				])),
+			]);
+		},
+	},
 ];
 
 vi.stubGlobal('crypto', webcrypto);
@@ -56,6 +94,16 @@ function toHex(buf: Uint8Array) {
 
 ciphers.map((cipher) =>
 	describe(cipher.name, () => {
+		test('Encrypted text may be decrypted', async () => {
+			const key = getRandomBytes(32);
+			const codec = await cipher.create(key, getRandomBytes);
+
+			const originalData = getRandomBytes(1024 * 1024);
+
+			const ct = await codec.encrypt(originalData.buffer.slice());
+			await expect(codec.decrypt(ct)).resolves.toStrictEqual(originalData.buffer);
+		});
+
 		test('Encrypted text may be decrypted by another instance', async () => {
 			const pt = getRandomBytes(1000);
 			const key = getRandomBytes(32);
@@ -67,37 +115,63 @@ ciphers.map((cipher) =>
 			await expect(cipher2.decrypt(ct)).resolves.toStrictEqual(pt.buffer);
 		});
 
-		test('Matches reference test vectors', async () => {
-			// We disable any random factor for that test
-			const randomBytesMock = (len: number) => new Uint8Array(len);
+		test('Messages is not equal to each other', async () => {
+			const key = getRandomBytes(32);
+			const codec = await cipher.create(key, getRandomBytes);
+			const originalData = getRandomBytes(1024).buffer;
 
-			const keySize = 32;
-			let key = fromHex('00000000000000000000000000000000');
-			let pt = fromHex('00000000000000000000000000000000').buffer;
+			// All cipher text is unique
+			const cipherTexts = new Set<ArrayBuffer>();
+			for (let i = 0; i < 100; i++) {
+				const ct = await codec.encrypt(originalData);
+				expect(cipherTexts.has(ct), 'Each cipher text are unique').toBe(false);
 
-			for (let i = 1; i <= 10; i++) {
-				const cipher1 = await cipher.create(key, randomBytesMock);
-				const ct1 = await cipher1.encrypt(pt);
-				expect({
-					key: toHex(key),
-					pt: toHex(new Uint8Array(pt)),
-					ct: toHex(new Uint8Array(ct1)),
-				}).toMatchSnapshot();
+				cipherTexts.add(ct);
+				expect(cipherTexts.has(ct), 'Ensure the cipher text is recorded').toBe(
+					true,
+				);
+			}
 
-				pt = ct1;
-
-				const cipher2 = await cipher.create(key, randomBytesMock);
-				const ct2 = await cipher2.encrypt(pt);
-				expect({
-					key: toHex(key),
-					pt: toHex(new Uint8Array(pt)),
-					ct: toHex(new Uint8Array(ct2)),
-				}).toMatchSnapshot();
-
-				// We take only last N bytes to use it as a valid key
-				key = new Uint8Array(ct1).slice(-keySize);
-				pt = ct2;
+			// All cipher texts must be decrypted to a buffer equal to original
+			for (const ct of cipherTexts) {
+				await expect(codec.decrypt(ct)).resolves.toStrictEqual(originalData);
 			}
 		});
+
+		// Verify vectors only for ciphers, not its combinations
+		if (!cipher.name.includes('-')) {
+			test('Matches reference test vectors', async () => {
+				// We disable any random factor for that test
+				const randomBytesMock = (len: number) => new Uint8Array(len);
+
+				const keySize = 32;
+				let key = fromHex('00000000000000000000000000000000');
+				let pt = fromHex('00000000000000000000000000000000').buffer;
+
+				for (let i = 1; i <= 10; i++) {
+					const cipher1 = await cipher.create(key, randomBytesMock);
+					const ct1 = await cipher1.encrypt(pt);
+					expect({
+						key: toHex(key),
+						pt: toHex(new Uint8Array(pt)),
+						ct: toHex(new Uint8Array(ct1)),
+					}).toMatchSnapshot();
+
+					pt = ct1;
+
+					const cipher2 = await cipher.create(key, randomBytesMock);
+					const ct2 = await cipher2.encrypt(pt);
+					expect({
+						key: toHex(key),
+						pt: toHex(new Uint8Array(pt)),
+						ct: toHex(new Uint8Array(ct2)),
+					}).toMatchSnapshot();
+
+					// We take only last N bytes to use it as a valid key
+					key = new Uint8Array(ct1).slice(-keySize);
+					pt = ct2;
+				}
+			});
+		}
 	}),
 );
