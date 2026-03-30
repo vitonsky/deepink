07073e9d93dc23cbfe69cb1317cf6b2b4385bf66
Irina Katsyuta <katsyuta@proton.me>
2026-03-30 20:48:25 +0200

    refactor: remove duplicated code

--- src/features/App/Workspace/useRestoreWorkspace.tsx
+++ src/features/App/Workspace/useRestoreWorkspace.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useVaultStorage } from '@features/files';
 import { getWorkspacePath } from '@features/files/paths';
+import { useUpdateNotes } from '@hooks/notes/useUpdateNotes';
 import { useAppDispatch } from '@state/redux/hooks';
 import { useWorkspaceActions, useWorkspaceData } from '@state/redux/profiles/hooks';
-import { NOTES_VIEW } from '@state/redux/profiles/profiles';
 
 import { createWorkspaceStateFiles } from './utils/createWorkspaceStateFiles';
 import { useWorkspaceErrorHandlerContext } from './WorkspaceErrorHandlerContext';
@@ -22,6 +22,10 @@ export const useRestoreWorkspace = () => {
 	const notesRegistry = useNotesRegistry();
 	const tagsRegistry = useTagsRegistry();
 	const workspaceStorage = useVaultStorage(getWorkspacePath(workspaceData.workspaceId));
+
+	const loadNotes = useUpdateNotes();
+	const loadNotesRef = useRef(loadNotes);
+	loadNotesRef.current = loadNotes;
 
 	useEffect(() => {
 		const { workspaceConfig, workspaceState } =
@@ -60,43 +64,31 @@ export const useRestoreWorkspace = () => {
 					);
 				}
 
-				// Get opened notes and notes list
-				const [noteIdList, openedNoteList] = await Promise.all([
-					notesRegistry.query({
-						tags: selectedTagId ? [selectedTagId] : [],
-						search: state?.search ? { text: state.search } : undefined,
-						sort: { by: 'updatedAt', order: 'desc' },
-						meta: {
-							isDeleted: state?.view === NOTES_VIEW.BIN,
-							// show archived notes only in archive view
-							// but do not filter by the archived flag in bin view
-							...(state?.view !== NOTES_VIEW.BIN && {
-								isArchived: state?.view === NOTES_VIEW.ARCHIVE,
-							}),
-							...(state?.view === NOTES_VIEW.BOOKMARK && {
-								isBookmarked: true,
-							}),
-						},
-					}),
+				// Restore opened notes
+				if (state && state.openedNoteIds && state.openedNoteIds.length > 0) {
+					const openedNoteList = await notesRegistry.getById(
+						state.openedNoteIds,
+					);
 
-					state?.openedNoteIds && state?.openedNoteIds.length > 0
-						? notesRegistry.getById(state.openedNoteIds)
-						: Promise.resolve(null),
-				]);
+					if (openedNoteList && openedNoteList.length > 0) {
+						dispatch(
+							workspaceActions.setOpenedNotes({ notes: openedNoteList }),
+						);
+
+						let activeNote = openedNoteList[0];
+						if (state.activeNoteId) {
+							activeNote =
+								openedNoteList.find((n) => n.id === state.activeNoteId) ||
+								openedNoteList[0];
+						}
+						dispatch(
+							workspaceActions.setActiveNote({ noteId: activeNote.id }),
+						);
+					}
+				}
 
 				// Restore notes list
-				dispatch(workspaceActions.setNoteIds({ noteIds: noteIdList }));
-
-				// Restore opened notes
-				if (openedNoteList && openedNoteList.length > 0) {
-					dispatch(workspaceActions.setOpenedNotes({ notes: openedNoteList }));
-
-					const activeNote =
-						(state?.activeNoteId &&
-							openedNoteList.find((n) => n.id === state.activeNoteId)) ||
-						openedNoteList[0];
-					dispatch(workspaceActions.setActiveNote({ noteId: activeNote.id }));
-				}
+				await loadNotesRef.current();
 
 				dispatch(
 					workspaceActions.setWorkspaceLoadingStatus({
