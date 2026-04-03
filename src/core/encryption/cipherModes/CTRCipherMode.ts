0aa01ce4e1f1f215a85411c122e11999fd0977b7
Robert Vitonsky <robert@vitonsky.net>
2026-04-03 15:04:56 +0200

    fix: join nonce || counter in CTR mode

--- src/core/encryption/cipherModes/CTRCipherMode.ts
+++ src/core/encryption/cipherModes/CTRCipherMode.ts
@@ -6,23 +6,20 @@ import { xor16 } from '../utils/xor';
  * RFC: https://datatracker.ietf.org/doc/html/rfc3686
  */
 export class CTRCipherMode {
-	private readonly encryptBuffer;
-	private readonly blockSize;
-	constructor(encryptBuffer: (buffer: Uint8Array) => Uint8Array, blockSize = 16) {
-		this.encryptBuffer = encryptBuffer;
-		this.blockSize = blockSize;
-	}
+	constructor(
+		private readonly encryptBuffer: (buffer: Uint8Array) => Uint8Array,
+		private readonly blockSize = 16,
+	) {}
 
 	public async encrypt(buffer: Uint8Array, iv: Uint8Array) {
 		if (buffer.byteLength % this.blockSize !== 0)
-			throw new TypeError(`Buffer size is not multiple to ${this.blockSize}`);
-		if (iv.byteLength % this.blockSize !== 0)
 			throw new TypeError(
-				`Initialization vector size is not multiple to ${this.blockSize} bytes`,
+				`Buffer size is not multiple to ${this.blockSize}. Did you missed to add padding?`,
 			);
-
-		const counterBuffer = new Uint8Array(this.blockSize);
-		const counterView = new DataView(counterBuffer.buffer);
+		if (iv.byteLength + 4 !== this.blockSize)
+			throw new TypeError(
+				`Initialization vector have size ${iv.byteLength} bytes + 4 byte for 32bit counter = ${iv.byteLength + 4} byte. It does not match a block size`,
+			);
 
 		const out = new Uint8Array(buffer.byteLength);
 
@@ -30,19 +27,20 @@ export class CTRCipherMode {
 		// since only one XOR buffer is used at once
 		const xorBuffer = new Uint8Array(16);
 
-		for (let offset = 0; offset < buffer.byteLength; offset += this.blockSize) {
-			// XOR a Nonce and Counter
-			const ivOffset = offset % iv.byteLength;
-			// TODO: provide an efficient xor function as dependency
-			const uniqueSequence = xor16(
-				xorBuffer,
-				iv.subarray(ivOffset, ivOffset + this.blockSize),
-				counterBuffer,
-			);
+		// Layout is `nonce || counter`
+		const input = new Uint8Array(this.blockSize);
+		input.set(iv);
+
+		const counterView = new DataView(input.buffer, this.blockSize - 4);
+
+		const blocksCount = Math.ceil(buffer.byteLength / this.blockSize);
+		for (let i = 0; i < blocksCount; ) {
+			const offset = i * this.blockSize;
 
 			// Encrypt unique sequence
-			const encryptedSequence = this.encryptBuffer(uniqueSequence);
+			const encryptedSequence = this.encryptBuffer(input);
 
+			// TODO: provide an efficient xor function as dependency
 			// XOR unique sequence and block data,
 			// a data is a plain text for encryption and cipher text for decryption
 			const dataBlock = xor16(
@@ -55,8 +53,7 @@ export class CTRCipherMode {
 			out.set(dataBlock, offset);
 
 			// Increment counter
-			const nextCounter = counterView.getUint32(0) + 1;
-			counterView.setUint32(0, nextCounter);
+			counterView.setUint32(0, ++i);
 		}
 
 		return out;
