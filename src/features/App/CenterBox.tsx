910328f7ac86219e86524643bbf996a937ce5c16
Irina Katsyuta <katsyuta@proton.me>
2026-04-08 12:44:39 +0200

    chore: remove typo

--- src/features/App/CenterBox.tsx
+++ src/features/App/CenterBox.tsx
@@ -5,7 +5,7 @@ export const CenterBox: FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<Center h="100vh">
 			<Box maxW="500px" minW="350px">
-				{children};
+				{children}
 			</Box>
 		</Center>
 	);
