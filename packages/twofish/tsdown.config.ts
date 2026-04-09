2a23bd1768672726c1dd35b8dd4bc8f7bac33fed
Robert Vitonsky <robert@vitonsky.net>
2026-04-09 16:13:30 +0200

    build: clean tsdown config

--- packages/twofish/tsdown.config.ts
+++ packages/twofish/tsdown.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
+	clean: false,
 	dts: true,
 	exports: {
 		customExports: {
 			'./twofish.wasm': './dist/twofish.wasm',
 		},
 	},
-	clean: false,
-	// ...config options
 });
