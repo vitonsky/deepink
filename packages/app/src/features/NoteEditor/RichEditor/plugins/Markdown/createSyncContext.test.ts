e2fda4353a20c6d93fb42630b7f98eeccc59ae68
Robert Vitonsky <robert@vitonsky.net>
2026-07-06 15:09:05 +0200

    test: cases for sync context

---
+++ packages/app/src/features/NoteEditor/RichEditor/plugins/Markdown/createSyncContext.test.ts
@@ -0,0 +1,47 @@
+import { createSyncContext } from './createSyncContext';
+
+test('Default value can be used anywhere', () => {
+	const ctx = createSyncContext(1);
+	expect(ctx.get()).toBe(1);
+});
+
+test('Overridden value can be accessed inside context', () => {
+	const ctx = createSyncContext(1);
+
+	expect.assertions(1);
+	ctx.use(2, () => {
+		expect(ctx.get()).toBe(2);
+	});
+});
+
+test('The nearest value must be accessible inside context', () => {
+	expect.assertions(3);
+
+	const ctx = createSyncContext(1);
+	ctx.use(2, () => {
+		ctx.use(3, () => {
+			expect(ctx.get()).toBe(3);
+
+			ctx.use(4, () => {
+				ctx.use(5, () => {
+					expect(ctx.get()).toBe(5);
+				});
+			});
+
+			expect(ctx.get()).toBe(3);
+		});
+	});
+});
+
+test('Context use can return value', () => {
+	const ctx = createSyncContext(1);
+	expect(
+		ctx.use(2, () => 'test'),
+		'Callback may return any value',
+	).toBe('test');
+
+	expect(
+		ctx.use(2, () => ctx.use(3, () => ctx.use(4, () => ctx.get()))),
+		'Callback may return its context value',
+	).toBe(4);
+});
