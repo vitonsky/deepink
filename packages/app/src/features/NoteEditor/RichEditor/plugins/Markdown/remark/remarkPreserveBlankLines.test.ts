eae00e540e4292e83794e1a65ae117ec5b56921b
Robert Vitonsky <robert@vitonsky.net>
2026-07-08 14:27:14 +0200

    feat: convert each 2 lines into 1 paragraph

---
+++ packages/app/src/features/NoteEditor/RichEditor/plugins/Markdown/remark/remarkPreserveBlankLines.test.ts
@@ -0,0 +1,49 @@
+import remarkParse from 'remark-parse';
+import { unified } from 'unified';
+
+import { fillGapsWithParagraphs } from './remarkPreserveBlankLines';
+
+export const markdownProcessor = unified().use(remarkParse);
+
+test('Must not to add paragraphs for text with no gaps', () => {
+	const tree = markdownProcessor.parse('Hello\nWorld');
+	expect(tree.children).toHaveLength(1);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(1);
+});
+
+test('Must not to add paragraphs for one line gap', () => {
+	const tree = markdownProcessor.parse('Hello\n\nWorld');
+	expect(tree.children).toHaveLength(2);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(2);
+});
+
+test('Must add 1 paragraph for gap in 2 lines', () => {
+	const tree = markdownProcessor.parse('Hello\n\n\nWorld');
+	expect(tree.children).toHaveLength(2);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(3);
+});
+
+test('Must add 5 paragraphs for gap in 10 lines', () => {
+	const tree = markdownProcessor.parse('Hello\n' + '\n'.repeat(10) + 'World');
+	expect(tree.children).toHaveLength(2);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(2 + 5);
+});
+
+// TODO: implement lines preservation
+test.fails('Preserve lines for document with no text', () => {
+	const tree = markdownProcessor.parse('\n'.repeat(10));
+	expect(tree.children).toHaveLength(0);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(5);
+});
+
+test.fails('Preserve lines at start', () => {
+	const tree = markdownProcessor.parse('\n'.repeat(10) + '\nText');
+	expect(tree.children).toHaveLength(1);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(6);
+});
+
+test.fails('Preserve lines at end', () => {
+	const tree = markdownProcessor.parse('Text\n' + '\n'.repeat(10));
+	expect(tree.children).toHaveLength(1);
+	expect(fillGapsWithParagraphs(tree).children).toHaveLength(6);
+});
--- packages/app/src/features/NoteEditor/RichEditor/plugins/Markdown/remark/remarkPreserveBlankLines.ts
+++ packages/app/src/features/NoteEditor/RichEditor/plugins/Markdown/remark/remarkPreserveBlankLines.ts
@@ -2,61 +2,67 @@ import { type Root, RootContent } from 'mdast';
 import { Plugin } from 'unified';
 import { CONTINUE, SKIP, visit } from 'unist-util-visit';
 
-export const remarkPreserveBlankLines: Plugin<[], Root> = () => {
-	const ignoredNodeTypes = new Set<string>([
-		'table',
-		'tableCell',
-		'tableRow',
-	] satisfies RootContent['type'][]);
+const ignoredNodeTypes = new Set<string>([
+	'table',
+	'tableCell',
+	'tableRow',
+] satisfies RootContent['type'][]);
 
-	return (tree: Root) => {
-		const skipNodes = new Set<unknown>();
+export const fillGapsWithParagraphs = (tree: Root) => {
+	const skipNodes = new Set<unknown>();
 
-		visit(tree, (node) => {
-			// Skip nodes with no nested elements
-			if (!('children' in node)) return SKIP;
+	visit(tree, (node) => {
+		// Skip nodes with no nested elements
+		if (!('children' in node)) return SKIP;
 
-			// Skip ignored node types
-			if (ignoredNodeTypes.has(node.type)) return SKIP;
+		// Skip ignored node types
+		if (ignoredNodeTypes.has(node.type)) return SKIP;
 
-			// Skip already handled nodes
-			if (skipNodes.has(node)) return SKIP;
+		// Skip already handled nodes
+		if (skipNodes.has(node)) return SKIP;
 
-			const newChildren: RootContent[] = [];
-			for (let i = 0; i < node.children.length; i++) {
-				const current = node.children[i];
-				const next = node.children[i + 1];
+		const newChildren: RootContent[] = [];
+		for (let i = 0; i < node.children.length; i++) {
+			const current = node.children[i];
+			const next = node.children[i + 1];
 
-				// Collect its own children
-				newChildren.push(current);
+			// Collect its own children
+			newChildren.push(current);
 
-				// Add empty lines to preserve
-				if (next && current.position && next.position) {
-					const lineGap = next.position.start.line - current.position.end.line;
-					// lineGap === 2 means exactly one blank line, 3 means two, etc.
-					const blankLineCount = lineGap - 1;
+			// Add empty lines to preserve
+			if (next && current.position && next.position) {
+				const lineGap = next.position.start.line - current.position.end.line;
+				// lineGap === 2 means exactly one blank line, 3 means two, etc.
+				const blankLineCount = lineGap - 1;
+				const paragraphsCount =
+					blankLineCount > 0 ? Math.floor(blankLineCount / 2) : 0;
 
-					for (let b = 0; b < blankLineCount; b++) {
-						const line = current.position.end.line + 1 + b;
-						const emptyLine = {
-							type: 'paragraph',
-							children: [],
-							// TODO: add tests to verify position are correct in complex cases
-							position: {
-								start: { line, column: 1, offset: 0 },
-								end: { line, column: 1, offset: 0 },
-							},
-						} as RootContent;
+				for (let b = 0; b < paragraphsCount; b++) {
+					const line = current.position.end.line + 1 + b;
+					const emptyLine = {
+						type: 'paragraph',
+						children: [],
+						// TODO: add tests to verify position are correct in complex cases
+						position: {
+							start: { line, column: 1, offset: 0 },
+							end: { line, column: 1, offset: 0 },
+						},
+					} as RootContent;
 
-						newChildren.push(emptyLine);
-						skipNodes.add(emptyLine);
-					}
+					newChildren.push(emptyLine);
+					skipNodes.add(emptyLine);
 				}
 			}
+		}
 
-			node.children = newChildren;
+		node.children = newChildren;
 
-			return CONTINUE;
-		});
-	};
+		return CONTINUE;
+	});
+
+	return tree;
+};
+
+export const remarkPreserveBlankLines: Plugin<[], Root> = () => {
+	return fillGapsWithParagraphs;
 };
